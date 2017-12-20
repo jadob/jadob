37b5e01794f5113ee4ddb59dbc8c1238dabf8941
pizzaminded <pizzaminded@users.noreply.github.com>
2017-12-20 23:31:42 +0100

    [Form] added communique

--- src/Slice/Form/Resources/functions/polyfills.php
+++ src/Slice/Form/Resources/functions/polyfills.php
@@ -1,5 +1,13 @@
 <?php
+/**
+ * This file is kinda of deprecated, but it will be available until i will find some better place for this stuff
+ */
 
+
+/**
+ * @param $word
+ * @return null|string|string[]
+ */
 function decamelize($word) {
     return preg_replace(
             '/(^|[a-z])([A-Z])/e', 'strtolower(strlen("\\1") ? "\\1_\\2" : "\\2")', $word
