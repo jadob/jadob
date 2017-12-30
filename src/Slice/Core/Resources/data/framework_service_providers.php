6cec1e647e2a0ba63bc80d77132a58a61608fd8e
pizzaminded <pizzaminded@users.noreply.github.com>
2017-12-30 05:12:05 +0100

    [Core] added Globals provider

--- src/Slice/Core/Resources/data/framework_service_providers.php
+++ src/Slice/Core/Resources/data/framework_service_providers.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * A complete list of services provided by framework. 
+ * A complete list of services provided by framework.
  */
 return [
     Slice\Core\ServiceProvider\TopLevelServicesProvider::class,
@@ -9,5 +9,6 @@ return [
     Slice\TwigBridge\ServiceProvider\TwigServiceProvider::class,
     Slice\DoctrineDBALBridge\ServiceProvider\DoctrineDBALServiceProvider::class,
     Slice\Database\ServiceProvider\DatabaseServiceProvider::class,
-   \Slice\Debug\ServiceProvider\DebugServiceProvider::class
+    \Slice\Debug\ServiceProvider\DebugServiceProvider::class,
+    \Slice\Core\ServiceProvider\GlobalVariablesProvider::class
 ];
