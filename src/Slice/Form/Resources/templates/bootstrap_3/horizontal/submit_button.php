89edc8a0ff4c7d1bed40546c03f6a42bab3c45a8
pizzaminded <pizzaminded@users.noreply.github.com>
2018-01-05 00:07:10 +0100

    [Form] removed grid properties
    Signed-off-by: pizzaminded <pizzaminded@users.noreply.github.com>

--- src/Slice/Form/Resources/templates/bootstrap_3/horizontal/submit_button.php
+++ src/Slice/Form/Resources/templates/bootstrap_3/horizontal/submit_button.php
@@ -1,9 +1,7 @@
 <div class="form-group">
-    <div class="col-sm-offset-2 col-sm-10">
-        <button class="btn btn-default"
-                type="<?= $input->getType(); ?>"
-                name="<?= $formName; ?>[<?= $input->getName(); ?>]"
-                value="<?= $input->getValue() ? $input->getValue() : '' ?>"
-        ><?= $input->getLabel(); ?></button>
-    </div>
+    <button class="btn btn-default"
+            type="<?= $input->getType(); ?>"
+            name="<?= $formName; ?>[<?= $input->getName(); ?>]"
+            value="<?= $input->getValue() ? $input->getValue() : '' ?>"
+    ><?= $input->getLabel(); ?></button>
 </div>
--- src/Slice/Form/Resources/templates/bootstrap_3/horizontal/text_input.php
+++ src/Slice/Form/Resources/templates/bootstrap_3/horizontal/text_input.php
@@ -7,18 +7,16 @@ $inputId = $formName . '_' . $input->getName();
 ?>
     <div class="form-group <?= $input->isValid() ? '' : 'has-error' ?>">
         <label for="<?= $inputId; ?>"
-               class="col-sm-2 text-muted control-label">
+               class="text-muted control-label">
             <?= $input->getLabel(); ?>
         </label>
-        <div class="col-sm-10">
-            <input
-                    id="<?= $inputId; ?>"
-                    type="text"
-                    name="<?= $formName; ?>[<?= $input->getName(); ?>]"
-                    value="<?= $input->getValue() ?: '' ?>"
-                    class="form-control"
-                    <?= $input->getRequired() ? 'required' : '' ?>
-                    placeholder="<?= $input->getPlaceholder(); ?>">
-        </div>
+        <input
+                id="<?= $inputId; ?>"
+                type="text"
+                name="<?= $formName; ?>[<?= $input->getName(); ?>]"
+                value="<?= $input->getValue() ?: '' ?>"
+                class="form-control"
+            <?= $input->getRequired() ? 'required' : '' ?>
+                placeholder="<?= $input->getPlaceholder(); ?>">
     </div>
 <?php unset($inputId);
