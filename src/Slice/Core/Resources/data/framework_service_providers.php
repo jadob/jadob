6042473408f31beb6f537071bfec79ec06d11952
pizzaminded <pizzaminded@users.noreply.github.com>
2018-01-24 18:56:51 +0100

    [Core] Changed order of providers
    Signed-off-by: pizzaminded <pizzaminded@users.noreply.github.com>

--- src/Slice/Core/Resources/data/framework_service_providers.php
+++ src/Slice/Core/Resources/data/framework_service_providers.php
@@ -6,13 +6,13 @@
 return [
     \Slice\Core\ServiceProvider\TopLevelServicesProvider::class,
     \Slice\Router\ServiceProvider\RouterServiceProvider::class,
-    \Slice\TwigBridge\ServiceProvider\TwigServiceProvider::class,
     \Slice\DoctrineDBALBridge\ServiceProvider\DoctrineDBALServiceProvider::class,
-    \Slice\SymfonyTranslationBridge\ServiceProvider\TranslationServiceProvider::class,
     \Slice\Database\ServiceProvider\DatabaseServiceProvider::class,
     \Slice\Debug\ServiceProvider\DebugServiceProvider::class,
     \Slice\Core\ServiceProvider\GlobalVariablesProvider::class,
     \Slice\Form\ServiceProvider\FormProvider::class,
     \Slice\Security\ServiceProvider\SecurityProvider::class,
-    \Slice\Cache\ServiceProvider\CacheProvider::class
+    \Slice\Cache\ServiceProvider\CacheProvider::class,
+    \Slice\TwigBridge\ServiceProvider\TwigServiceProvider::class,
+    \Slice\SymfonyTranslationBridge\ServiceProvider\TranslationServiceProvider::class,
 ];
