d8eaa443de7956a1c3ff5c2449a091e5ad23aea0
pizzaminded <pizzaminded@users.noreply.github.com>
2018-03-03 21:57:10 +0100

    [Core] added framework bootstrapping conventions

---
+++ src/Jadob/Core/AbstractBootstrap.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Jadob\Core;
+
+/**
+ * There are some required methods implemented.
+ * You can extend your bootstrap class by this one to get some job done.
+ * By default, Jadob uses php-pds standards (@see https://github.com/php-pds/skeleton)
+ * but you can override this to your own needs.
+ * @package Jadob\Core
+ * @author pizzaminded <miki@appvende.net>
+ * @license MIT
+ */
+abstract class AbstractBootstrap implements BootstrapInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getPublicDir(): string
+    {
+        return $this->getRootDir() . '/public';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigDir(): string
+    {
+        return $this->getRootDir() . '/config';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCacheDir(): string
+    {
+        return $this->getRootDir() . '/var';
+    }
+}
