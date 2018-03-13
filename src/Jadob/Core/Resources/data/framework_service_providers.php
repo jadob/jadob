c8fb36ed12b8b7547bf90f0d04b57d46e496e14d
pizzaminded <pizzaminded@users.noreply.github.com>
2018-03-13 21:06:39 +0100

    [Core][CommandBus] added new provider

--- src/Jadob/Core/Resources/data/framework_service_providers.php
+++ src/Jadob/Core/Resources/data/framework_service_providers.php
@@ -9,10 +9,10 @@ return [
     \Jadob\Router\ServiceProvider\RouterServiceProvider::class,
     \Jadob\DoctrineDBALBridge\ServiceProvider\DoctrineDBALServiceProvider::class,
     \Jadob\Database\ServiceProvider\DatabaseServiceProvider::class,
-//    \Jadob\Debug\ServiceProvider\DebugServiceProvider::class,
     \Jadob\Form\ServiceProvider\FormProvider::class,
     \Jadob\Security\ServiceProvider\SecurityProvider::class,
     \Jadob\Cache\ServiceProvider\CacheProvider::class,
     \Jadob\TwigBridge\ServiceProvider\TwigServiceProvider::class,
     \Jadob\SymfonyTranslationBridge\ServiceProvider\TranslationServiceProvider::class,
+    \Jadob\CommandBus\ServiceProvider\CommandBusProvider::class
 ];
