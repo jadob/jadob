d6642858fcab9ebbdc9e906ac8f3279e21fe1eb5
pizzaminded <pizzaminded@users.noreply.github.com>
2018-03-19 02:58:10 +0100

    [Form] fixed phpdocs

--- src/Jadob/Form/Field/FormFieldInterface.php
+++ src/Jadob/Form/Field/FormFieldInterface.php
@@ -1,19 +1,18 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace Jadob\Form\Field;
 
 /**
- *
+ * Interface FormFieldInterface
+ * @package Jadob\Form\Field
  * @author pizzaminded <miki@appvende.net>
  */
 interface FormFieldInterface {
-    //put your code here
 
+    /**
+     * Creates form input from config passed to $data.
+     * @param $data
+     * @return FormFieldInterface
+     */
     public static function fromArray($data);
 }
