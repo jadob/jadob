d816e4eaee6ca52fcfaa9618d2a82ed0d23ce731
pizzaminded <pizzaminded@users.noreply.github.com>
2018-04-11 03:44:07 +0200

    [Core] reordered providers

--- src/Jadob/Core/Resources/data/framework_service_providers.php
+++ src/Jadob/Core/Resources/data/framework_service_providers.php
@@ -4,16 +4,17 @@
  * A complete list of services provided by framework.
  */
 return [
+    \Jadob\SymfonyConsoleBridge\ServiceProvider\ConsoleProvider::class,
     \Jadob\Core\ServiceProvider\TopLevelServicesProvider::class,
     \Jadob\Core\ServiceProvider\GlobalVariablesProvider::class,
     \Jadob\Router\ServiceProvider\RouterServiceProvider::class,
     \Jadob\DoctrineDBALBridge\ServiceProvider\DoctrineDBALServiceProvider::class,
     \Jadob\Database\ServiceProvider\DatabaseServiceProvider::class,
-    \Jadob\Form\ServiceProvider\FormProvider::class,
     \Jadob\Security\ServiceProvider\SecurityProvider::class,
-    \Jadob\Security\ServiceProvider\FirewallProvider::class,
-    \Jadob\Cache\ServiceProvider\CacheProvider::class,
     \Jadob\TwigBridge\ServiceProvider\TwigServiceProvider::class,
     \Jadob\SymfonyTranslationBridge\ServiceProvider\TranslationServiceProvider::class,
+    \Jadob\Form\ServiceProvider\FormProvider::class,
+    \Jadob\Security\ServiceProvider\FirewallProvider::class,
+    \Jadob\Cache\ServiceProvider\CacheProvider::class,
     \Jadob\CommandBus\ServiceProvider\CommandBusProvider::class
 ];
