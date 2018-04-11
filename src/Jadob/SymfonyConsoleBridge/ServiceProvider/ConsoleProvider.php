a380cf98c0f171d798277c381646b2370dbfbc59
pizzaminded <pizzaminded@users.noreply.github.com>
2018-04-11 03:46:10 +0200

    [SymfonyConsoleBridgeProvider] added console provider

---
+++ src/Jadob/SymfonyConsoleBridge/ServiceProvider/ConsoleProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Jadob\SymfonyConsoleBridge\ServiceProvider;
+
+use Jadob\Container\Container;
+use Jadob\Container\ServiceProvider\ServiceProviderInterface;
+use Jadob\Core\Kernel;
+use Jadob\Stdlib\StaticEnvironmentUtils;
+use Symfony\Component\Console\Application;
+
+/**
+ * Adds symfony/console functionality to framework.
+ * @see https://symfony.com/doc/3.4/components/console.html
+ * @package Jadob\SymfonyConsoleBridge\ServiceProvider
+ * @author pizzaminded <miki@appvende.net>
+ * @license MIT
+ */
+class ConsoleProvider implements ServiceProviderInterface
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigNode()
+    {
+        // TODO: Implement getConfigNode() method.
+    }
+
+    /**
+     * @param Container $container
+     * @param null $config
+     */
+    public function register(Container $container, $config)
+    {
+        if (StaticEnvironmentUtils::isCli()) {
+            $container->add('console', new Application('Jadob', Kernel::VERSION));
+        }
+    }
+}
