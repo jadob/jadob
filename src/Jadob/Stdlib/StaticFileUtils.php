8b5107d4e195c51000588c3d8c7a8d285bb6210f
pizzaminded <pizzaminded@users.noreply.github.com>
2018-04-11 21:35:32 +0200

    [Stdlib] check if dir exists before removing

--- src/Jadob/Stdlib/StaticFileUtils.php
+++ src/Jadob/Stdlib/StaticFileUtils.php
@@ -6,6 +6,10 @@ class StaticFileUtils
 {
     public static function recursiveRemoveDirectory($path)
     {
+        if (!file_exists($path)) {
+            return;
+        }
+
         if (is_dir($path)) {
             array_map([__CLASS__, 'recursiveRemoveDirectory'], glob($path . DIRECTORY_SEPARATOR . '{,.[!.]}*', GLOB_BRACE));
             @rmdir($path);
