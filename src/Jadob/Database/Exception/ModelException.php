1b3d2ccf7d84404c0bda9d92e3099854d4001808
pizzaminded <pizzaminded@users.noreply.github.com>
2018-06-01 23:02:36 +0200

    [Database] Added ModelException

---
+++ src/Jadob/Database/Exception/ModelException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Jadob\Database\Exception;
+
+/**
+ * Class ModelException
+ * @package Jadob\Database\Exception
+ * @author pizzaminded <miki@appvende.net>
+ * @license MIT
+ */
+class ModelException extends \Exception
+{
+
+}
