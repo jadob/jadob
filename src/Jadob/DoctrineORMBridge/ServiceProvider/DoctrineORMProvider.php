c0e3434f1109ddd91d697edb99d136dd2ec1c1a8
pizzaminded <pizzaminded@users.noreply.github.com>
2018-07-04 21:12:47 +0200

    [orm] improved orm provider

--- src/Jadob/DoctrineORMBridge/ServiceProvider/DoctrineORMProvider.php
+++ src/Jadob/DoctrineORMBridge/ServiceProvider/DoctrineORMProvider.php
@@ -2,8 +2,14 @@
 
 namespace Jadob\DoctrineORMBridge\ServiceProvider;
 
+use Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\Common\Annotations\CachedReader;
+use Doctrine\Common\Cache\ArrayCache;
+use Doctrine\Common\Cache\PhpFileCache;
 use Doctrine\DBAL\Tools\Console\Helper\ConnectionHelper;
+use Doctrine\ORM\Configuration;
 use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
 use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
 use Doctrine\ORM\Tools\Setup;
@@ -32,17 +38,82 @@ class DoctrineORMProvider implements ServiceProviderInterface
 
     /**
      * {@inheritdoc}
+     * @throws \RuntimeException
      * @throws \InvalidArgumentException
      * @throws \Doctrine\ORM\ORMException
      * @throws \Jadob\Container\Exception\ServiceNotFoundException
      */
     public function register(Container $container, $config)
     {
-        $configuration = Setup::createAnnotationMetadataConfiguration(
-            $config['entity_paths'],
-            !$container->get('kernel')->isProduction()
-        );
+        /**
+         * Entity paths must be defined, otherwise there is no sense to load rest of ORM
+         */
+        if (!isset($config['entity_paths'])) {
+            throw new \RuntimeException('Entity paths is not defined,');
+        }
 
+        /**
+         * Paths should be relative, beginning from project root dir.
+         * Rest of path will be concatenated below.
+         */
+        $entityPaths = [];
+
+        foreach ($config['entity_paths'] as $path) {
+            //@TODO: trim beginning slash from any $path if present
+            $entityPaths[] = $container->get('bootstrap')->getRootDir() . '/' . $path;
+        }
+
+        $isDevMode = !$container->get('kernel')->isProduction();
+        $cacheDir = $container->get('bootstrap')->getCacheDir()
+            . '/'
+            . $container->get('kernel')->getEnv()
+            . '/doctrine';
+
+
+        /**
+         * Add cache to ORM.
+         * file - PhpFileCache
+         * filesystem - FilesystemCache
+         */
+        if (isset($config['cache'])) {
+
+            if (!isset($config['cache']['type'])) {
+                throw new \RuntimeException('Cache type is not provided.');
+            }
+
+            $cacheConfig = $config['cache'];
+
+            switch (strtolower($cacheConfig['type'])) {
+                case 'file':
+                    $cache = new PhpFileCache($cacheDir.'/cache');
+                    break;
+                default:
+                    $cache = new ArrayCache();
+                    break;
+            }
+
+        }
+
+        $this->registerAnnotations();
+
+        $configuration = new Configuration();
+        $configuration->setMetadataCacheImpl($cache);
+        $configuration->setHydrationCacheImpl($cache);
+        $configuration->setQueryCacheImpl($cache);
+        $configuration->setMetadataDriverImpl(
+            new AnnotationDriver(
+                new CachedReader(new AnnotationReader(), $cache),
+                $entityPaths
+            )
+        );
+        $configuration->setProxyNamespace('Doctrine\ORM\Proxies');
+        $configuration->setProxyDir($cacheDir . '/Doctrine/ORM/Proxies');
+        $configuration->setAutoGenerateProxyClasses(!$isDevMode);
+
+
+        /**
+         * Build EntityManager
+         */
         $entityManager = EntityManager::create(
             $container->get('doctrine.dbal'),
             $configuration
@@ -53,6 +124,10 @@ class DoctrineORMProvider implements ServiceProviderInterface
         if (!StaticEnvironmentUtils::isCli()) {
             return;
         }
+
+        /**
+         * If in CLI mode, add console helper
+         */
 
         /** @var Application $console */
         $console = $container->get('console');
@@ -66,6 +141,16 @@ class DoctrineORMProvider implements ServiceProviderInterface
         $console->setHelperSet($helperSet);
 
         ConsoleRunner::addCommands($console);
-
     }
+
+    /**
+     * @throws \ReflectionException
+     */
+    protected function registerAnnotations()
+    {
+        $configurationClassDirectory = \dirname((new \ReflectionClass(Configuration::class))->getFileName());
+
+        require_once $configurationClassDirectory . '/Mapping/Driver/DoctrineAnnotations.php';
+    }
+
 }
