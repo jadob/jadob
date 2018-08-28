e4c9bac0d8060c23a7738e213b3080a1340a3e92
pizzaminded <pizzaminded@users.noreply.github.com>
2018-08-28 23:49:38 +0200

    [core] unregistered unnecessary providers (who needs twig in ajax-only app? :DD)

--- src/Jadob/Core/Resources/data/framework_service_providers.php
+++ src/Jadob/Core/Resources/data/framework_service_providers.php
@@ -2,6 +2,7 @@
 
 /**
  * A complete list of services provided by framework.
+ * @author pizzaminded <miki@appvende.net>
  */
 return [
     \Jadob\SymfonyConsoleBridge\ServiceProvider\ConsoleProvider::class,
@@ -10,10 +11,13 @@ return [
     \Jadob\Router\ServiceProvider\RouterServiceProvider::class,
     \Jadob\DoctrineDBALBridge\ServiceProvider\DoctrineDBALServiceProvider::class,
     \Jadob\Database\ServiceProvider\DatabaseServiceProvider::class,
-    \Jadob\Security\ServiceProvider\SecurityProvider::class,
-    \Jadob\TwigBridge\ServiceProvider\TwigServiceProvider::class,
-    \Jadob\SymfonyTranslationBridge\ServiceProvider\TranslationServiceProvider::class,
-    \Jadob\Form\ServiceProvider\FormProvider::class,
+    \Jadob\Security\Auth\ServiceProvider\AuthProvider::class,
+    /**
+     * these three things below should be moved to Bootstrap class as it arent needed in eg. REST services
+     */
+//    \Jadob\TwigBridge\ServiceProvider\TwigServiceProvider::class,
+//    \Jadob\SymfonyTranslationBridge\ServiceProvider\TranslationServiceProvider::class,
+//    \Jadob\Form\ServiceProvider\FormProvider::class,
     \Jadob\Security\ServiceProvider\FirewallProvider::class,
     \Jadob\Cache\ServiceProvider\CacheProvider::class,
     \Jadob\CommandBus\ServiceProvider\CommandBusProvider::class
