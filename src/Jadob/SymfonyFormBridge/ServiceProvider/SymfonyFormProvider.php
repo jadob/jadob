eef78aa929e9aaaba15002e7b8d0fcdd4c7bc48f
pizzaminded <pizzaminded@users.noreply.github.com>
2018-11-23 20:06:38 +0100

    [SymfonyFormProvider] improved provider

--- src/Jadob/SymfonyFormBridge/ServiceProvider/SymfonyFormProvider.php
+++ src/Jadob/SymfonyFormBridge/ServiceProvider/SymfonyFormProvider.php
@@ -3,27 +3,22 @@
 namespace Jadob\SymfonyFormBridge\ServiceProvider;
 
 use Jadob\Container\Container;
+use Jadob\Container\ContainerBuilder;
 use Jadob\Container\ServiceProvider\ServiceProviderInterface;
 use Symfony\Bridge\Twig\Extension\FormExtension;
 use Symfony\Bridge\Twig\Extension\TranslationExtension;
-use Symfony\Bridge\Twig\Form\TwigRenderer;
 use Symfony\Bridge\Twig\Form\TwigRendererEngine;
 use Symfony\Component\Form\Extension\Csrf\CsrfExtension;
 use Symfony\Component\Form\Extension\HttpFoundation\HttpFoundationExtension;
-use Symfony\Component\Form\Extension\Validator\ValidatorExtension;
 use Symfony\Component\Form\FormRenderer;
 use Symfony\Component\Form\Forms;
-use Symfony\Component\Security\Csrf\CsrfTokenManager;
-use Symfony\Component\Security\Csrf\TokenGenerator\UriSafeTokenGenerator;
-use Symfony\Component\Security\Csrf\TokenStorage\SessionTokenStorage;
 use Symfony\Component\Translation\Loader\XliffFileLoader;
 use Symfony\Component\Translation\Translator;
 use Symfony\Component\Validator\Validation;
-use Symfony\Component\Validator\ValidatorBuilder;
 
 /**
  * Class SymfonyFormProvider
- * @package Jadobable\SymfonyFormBridge\ServiceProvider
+ * @package Jadob\SymfonyFormBridge\ServiceProvider
  * @author pizzaminded <miki@appvende.net>
  * @license proprietary
  */
@@ -44,7 +39,26 @@ class SymfonyFormProvider implements ServiceProviderInterface
      * @throws \ReflectionException
      * @throws \Jadob\Container\Exception\ServiceNotFoundException
      */
-    public function register(Container $container, $config)
+    public function register(ContainerBuilder $container, $config)
+    {
+
+
+    }
+
+    /**
+     * Stuff that's needed to be done after container is built.
+     * What can you do using these method?
+     * - This one gets container as a first argument, so, you can e.g. get all services implementing SomeCoolInterface,
+     * and inject them somewhere
+     * (example 1: using Twig, you can register all extensions)
+     * (example 2: EventListener registers all Listeners here)
+     * - You can add new services of course
+     *
+     * @param Container $container
+     * @param array|null $config the same config node as passed in register()
+     * @return void
+     */
+    public function onContainerBuild(Container $container, $config)
     {
         /** @var \Twig\Environment $twig */
         $twig = $container->get('twig');
@@ -52,17 +66,22 @@ class SymfonyFormProvider implements ServiceProviderInterface
         /** @var Translator $translator */
         $translator = $container->get('translator');
 
-        $translator->addLoader('xlf',new XliffFileLoader());
+        $translator->addLoader('xlf', new XliffFileLoader());
 
-        $validatorPath = \dirname((new \ReflectionClass(Validation::class))->getFileName()).'/Resources/translations/validators.pl.xlf';
+//        $validatorPath = \dirname((new \ReflectionClass(Validation::class))->getFileName()) . '/Resources/translations/validators.pl.xlf';
 
-        $translator->addResource('xlf', $validatorPath, $translator->getLocale());
+//        $translator->addResource('xlf', $validatorPath, $translator->getLocale());
 
-        $validatorBuilder = new ValidatorBuilder();
-        $validatorBuilder->setTranslator($container->get('translator'));
-        $validator = $validatorBuilder->getValidator();
 
         $formEngine = new TwigRendererEngine($config['forms'], $twig);
+
+
+        $twig->addRuntimeLoader(new \Twig_FactoryRuntimeLoader([
+            FormRenderer::class => function () use ($formEngine) {
+                return new FormRenderer($formEngine);
+            },
+        ]));
+
 
         $twig->addExtension(new FormExtension());
         $twig->addExtension(
@@ -70,30 +89,14 @@ class SymfonyFormProvider implements ServiceProviderInterface
                 $container->get('translator')
             )
         );
-        $twig->addRuntimeLoader(new \Twig_FactoryRuntimeLoader([
-            FormRenderer::class => function () use ($formEngine) {
-                return new FormRenderer($formEngine);
-            },
-        ]));
 
-        $twig->addRuntimeLoader(new \Twig_FactoryRuntimeLoader([
-            FormRenderer::class => function () use ($formEngine) {
-                return new TwigRenderer($formEngine);
-            },
-        ]));
-
-        $csrfGenerator = new UriSafeTokenGenerator();
-        $csrfStorage = new SessionTokenStorage($container->get('session'));
-        $csrfManager = new CsrfTokenManager($csrfGenerator, $csrfStorage);
 
         $container->add('sf.forms', Forms::createFormFactoryBuilder()
-            ->addExtension(new HttpFoundationExtension()) //This will be needed as we use http-foundation component
-            ->addExtension(new ValidatorExtension($validator))
-            ->addExtension(new CsrfExtension($csrfManager))
+            ->addExtension(new HttpFoundationExtension())//This will be needed as we use http-foundation component
+//            ->addExtension(new ValidatorExtension($validator))
+            ->addExtension(new CsrfExtension($container->get('symfony.csrf.token.manager')))
         );
 
-        $container->add('sf.form.factory', $container->get('sf.forms')->getFormFactory());
-
-
+        $container->add('symfony.form.factory', $container->get('sf.forms')->getFormFactory());
     }
 }
