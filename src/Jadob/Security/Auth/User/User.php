0342e1751eb7d4302745e60448b3dbce6c512b7b
pizzaminded <pizzaminded@users.noreply.github.com>
2019-02-24 20:14:36 +0100

    [Auth] fixed default user class

--- src/Jadob/Security/Auth/User/User.php
+++ src/Jadob/Security/Auth/User/User.php
@@ -2,7 +2,7 @@
 
 namespace Jadob\Security\Auth\User;
 
-use Jadob\Security\Auth\UserInterface;
+use Jadob\Security\Auth\User\UserInterface;
 
 /**
  * Example User object, that can be used in your app.
@@ -13,4 +13,85 @@ use Jadob\Security\Auth\UserInterface;
 class User implements UserInterface
 {
 
+    /**
+     * @var string[]
+     */
+    protected $roles;
+
+    /**
+     * @var string
+     */
+    protected $username;
+
+    /**
+     * @var string
+     */
+    protected $password;
+
+    /**
+     * User constructor.
+     * @param string $username
+     * @param string $password
+     * @param array $roles
+     */
+    public function __construct($username, $password, $roles = [])
+    {
+        $this->username = $username;
+        $this->password = $password;
+        $this->roles = $roles;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getRoles(): array
+    {
+        return $this->roles;
+    }
+
+    /**
+     * @param string[] $roles
+     * @return User
+     */
+    public function setRoles(array $roles): User
+    {
+        $this->roles = $roles;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param string $username
+     * @return User
+     */
+    public function setUsername(string $username): User
+    {
+        $this->username = $username;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     * @return User
+     */
+    public function setPassword(string $password): User
+    {
+        $this->password = $password;
+        return $this;
+    }
 }
