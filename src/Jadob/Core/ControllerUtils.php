7227f6c0b7ba1750f94b0f92617c3d0a59e5fff3
pizzaminded <pizzaminded@users.noreply.github.com>
2019-03-02 21:46:56 +0100

    [Core] Added new method in ControllerUtils class

--- src/Jadob/Core/ControllerUtils.php
+++ src/Jadob/Core/ControllerUtils.php
@@ -5,6 +5,7 @@ namespace Jadob\Core;
 use Jadob\Security\Auth\User\UserInterface;
 use Psr\Container\ContainerInterface;
 use Symfony\Component\Form\FormFactory;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 /**
  * Class ControllerUtils
@@ -29,7 +30,11 @@ class ControllerUtils
         $this->container = $container;
     }
 
-
+    /**
+     * @param string $templateName
+     * @param array $data
+     * @return string
+     */
     public function renderTemplate($templateName, $data = [])
     {
         return $this->container->get('twig')->render($templateName, $data);
@@ -43,7 +48,6 @@ class ControllerUtils
         return $this->container->get('symfony.form.factory');
     }
 
-
     /**
      * @return UserInterface
      */
@@ -52,14 +56,34 @@ class ControllerUtils
         return $this->container->get('auth.user.storage')->getUser();
     }
 
+    /**
+     * @param string $type
+     * @param string $message
+     */
     public function addFlash($type, $message)
     {
         $this->container->get('session')->getFlashBag()->add($type, $message);
     }
 
+    /**
+     * @param string $name
+     * @param array $params
+     * @param bool $full
+     * @return mixed
+     */
     public function generateRoute($name, array $params = [], $full = false)
     {
         return $this->container->get('router')->generateRoute($name, $params, $full);
+    }
+
+    /**
+     * @param string $name
+     * @param array $params
+     * @return RedirectResponse
+     */
+    public function createRedirectToRouteResponse($name, $params = [])
+    {
+        return new RedirectResponse($this->generateRoute($name, $params));
     }
 
 }
