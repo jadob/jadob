2c364f3286dab4ba4895376c5fdca980b2e35ec4
pizzaminded <pizzaminded@users.noreply.github.com>
2019-04-01 14:18:23 +0200

    [Container] Added AbstractServiceProvider

---
+++ src/Jadob/Container/ServiceProvider/AbstractServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Jadob\Container\ServiceProvider;
+
+use Jadob\Container\Container;
+
+/**
+ * Class AbstractServiceProvider
+ * @package Jadob\Container\ServiceProvider
+ * @author pizzaminded <miki@appvende.net>
+ * @license MIT
+ */
+abstract class AbstractServiceProvider implements ServiceProviderInterface
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function register($config)
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function onContainerBuild(Container $container, $config)
+    {
+
+    }
+}
