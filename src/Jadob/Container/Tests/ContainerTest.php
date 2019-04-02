1d2f6f32163e15cccf0cc012310324e2237d05cb
pizzaminded <pizzaminded@users.noreply.github.com>
2019-04-02 22:26:56 +0200

    [Container] added some unit tests

--- src/Jadob/Container/Tests/ContainerTest.php
+++ src/Jadob/Container/Tests/ContainerTest.php
@@ -4,6 +4,7 @@ namespace Jadob\Container\Tests;
 
 use Jadob\Container\Container;
 use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
 
 /**
  * Class ContainerTest
@@ -32,5 +33,53 @@ class ContainerTest extends TestCase
         $container->alias('dummy', 'dummy1');
 
         $this->assertSame($container->get('dummy'), $container->get('dummy1'));
+    }
+
+    public function testAccessingFactories()
+    {
+
+        $container = new Container(['dummy1' => new \stdClass()], ['dummy2' => function () {
+            return new \stdClass();
+        }]);
+
+        $this->assertTrue($container->has('dummy1'));
+        $this->assertTrue($container->has('dummy2'));
+
+        $this->assertNotNull($container->get('dummy1'));
+        $this->assertNotNull($container->get('dummy2'));
+
+
+    }
+
+    /**
+     * @expectedException \Jadob\Container\Exception\ServiceNotFoundException
+     */
+    public function testServiceNotFoundException()
+    {
+        $container = new Container();
+
+        $container->get('missing');
+
+    }
+
+    public function testContainerIsPsr3Compatible()
+    {
+        $container = new Container();
+
+        $this->assertInstanceOf(ContainerInterface::class, $container);
+    }
+
+
+    public function testGettingServicesByItsClassName()
+    {
+        $container = new Container([], ['dummy2' => function () {
+            return new DummyClass();
+        }]);
+
+        $container->add('dummy', new \stdClass());
+
+        $this->assertInstanceOf(Container::class, $container->findObjectByClassName(ContainerInterface::class));
+        $this->assertInstanceOf(DummyClass::class, $container->findObjectByClassName(DummyClass::class));
+        $this->assertInstanceOf(\stdClass::class, $container->findObjectByClassName(\stdClass::class));
     }
 }
---
+++ src/Jadob/Container/Tests/DummyClass.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Jadob\Container\Tests;
+
+/**
+ * Class Used to test dependency container.
+ * @package Jadob\Container\Tests
+ * @author pizzaminded <miki@appvende.net>
+ * @license MIT
+ */
+class DummyClass
+{
+
+}
