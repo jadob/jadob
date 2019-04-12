7979da52f9dfc5a45018e9d8bd840d278451b3b4
pizzaminded <pizzaminded@users.noreply.github.com>
2019-04-12 14:44:37 +0200

    [Router] added Context object

---
+++ src/Jadob/Router/Context.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Jadob\Router;
+
+/**
+ * Class Context
+ * @package Jadob\Router
+ * @author pizzaminded <miki@appvende.net>
+ * @license MIT
+ */
+class Context
+{
+
+    /**
+     * @var string
+     */
+    protected $host;
+
+    /**
+     * @var bool
+     */
+    protected $secure = false;
+
+    /**
+     * @var int|null
+     */
+    protected $port;
+
+    /**
+     * @return Context
+     */
+    public static function fromGlobals()
+    {
+        return (new self())
+            ->setHost($_SERVER['HTTP_HOST'])
+            ->setPort($_SERVER['SERVER_PORT'])
+            ->setSecure(isset($_SERVER['HTTPS']));
+    }
+
+    /**
+     * @return string
+     */
+    public function getHost(): string
+    {
+        return $this->host;
+    }
+
+    /**
+     * @param string $host
+     * @return Context
+     */
+    public function setHost(string $host): Context
+    {
+        $this->host = $host;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isSecure(): bool
+    {
+        return $this->secure;
+    }
+
+    /**
+     * @param bool $secure
+     * @return Context
+     */
+    public function setSecure(bool $secure): Context
+    {
+        $this->secure = $secure;
+        return $this;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getPort(): ?int
+    {
+        return $this->port;
+    }
+
+    /**
+     * @param int $port
+     * @return Context
+     */
+    public function setPort(int $port): Context
+    {
+        $this->port = $port;
+        return $this;
+    }
+}
---
+++ src/Jadob/Router/Tests/ContextTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Jadob\Router\Tests;
+
+use Jadob\Router\Context;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class ContextTest
+ * @package Jadob\Router\Tests
+ * @author pizzaminded <miki@appvende.net>
+ * @license MIT
+ */
+class ContextTest extends TestCase
+{
+
+    public function testBasicContextFeatures()
+    {
+        $context = new Context();
+
+        $context->setSecure(true);
+        $context->setPort(1234);
+        $context->setHost('example.com');
+
+        $this->assertTrue($context->isSecure());
+        $this->assertEquals('example.com', $context->getHost());
+        $this->assertEquals(1234, $context->getPort());
+    }
+
+    public function testCreatingContextObjectFromSuperglobalArrays()
+    {
+        $_SERVER['HTTP_HOST'] = 'my.domain.com';
+        $_SERVER['HTTPS'] = true;
+        $_SERVER['SERVER_PORT'] = 8001;
+
+
+        $context = Context::fromGlobals();
+
+        $this->assertTrue($context->isSecure());
+        $this->assertEquals('my.domain.com', $context->getHost());
+        $this->assertEquals(8001, $context->getPort());
+    }
+}
