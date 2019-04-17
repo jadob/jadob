6580ee50410b5550b59b78f8232b4700a41d6cf2
pizzaminded <pizzaminded@users.noreply.github.com>
2019-04-17 23:44:31 +0200

    [Container] Added Exception

---
+++ src/Jadob/Container/Exception/ContainerBuildException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Jadob\Container\Exception;
+
+/**
+ * Class ContainerBuildException
+ * @package Jadob\Container\Exception
+ * @author pizzaminded <miki@appvende.net>
+ * @license MIT
+ */
+class ContainerBuildException extends \Exception
+{
+
+}
