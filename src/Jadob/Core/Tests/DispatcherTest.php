7b73030fd462b1b36e4b6b9cb8735e12355c85c9
pizzaminded <pizzaminded@users.noreply.github.com>
2019-04-17 23:55:44 +0200

    [Core] DispatcherTest in progress

--- src/Jadob/Core/Tests/DispatcherTest.php
+++ src/Jadob/Core/Tests/DispatcherTest.php
@@ -3,12 +3,12 @@
 namespace Jadob\Core\Tests;
 
 use Jadob\Container\Container;
-use Jadob\Core\Exception\KernelException;
-use Jadob\Core\Kernel;
+use Jadob\Core\Dispatcher;
 use Jadob\Router\Route;
 use Jadob\Router\RouteCollection;
 use Jadob\Router\Router;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -38,29 +38,42 @@ class ExampleController
 class DispatcherTest extends TestCase
 {
 
-    /**
-     * @throws KernelException
-     * @throws \Exception
-     */
-    public function testDispatcherWillExecuteOnlyClassesAndClosures()
+    public function setUp()
     {
+
+        $_SERVER['HTTP_HOST'] = 'localhost';
+        $_SERVER['SERVER_PORT'] = 8000;
+    }
+
+    /**
+     * @expectedException \Jadob\Core\Exception\KernelException
+     */
+    public function testDispatcherWillThrowExceptionIfControllerWillBeNull()
+    {
+
         $container = new Container();
 
-
         $invalidRoute = new Route('example_route_1', '/invalid', null);
-        $validRouteWithClosure = new Route('example_route_2', '/valid/1', function () {
-            return new Response('ok');
-        });
-
-        $validRouteWithClass = new Route('example_route_3', '/valid/2', ExampleController::class);
+//        $validRouteWithClosure = new Route('example_route_2', '/valid/1', function () {
+//            return new Response('ok');
+//        });
+//
+//        $validRouteWithClass = new Route('example_route_3', '/valid/2', ExampleController::class);
 
 
         $collection = new RouteCollection();
 
-        $collection->addRoute($invalidRoute)->addRoute($validRouteWithClosure)->addRoute($validRouteWithClass);
+        $collection->addRoute($invalidRoute);
 
+        $router = new Router($collection);
+        $container->add('router', $router);
 
+        $request = Request::create('/invalid');
 
+        $dispatcher = new Dispatcher($container);
 
+        $dispatcher->executeRequest($request);
     }
+
+
 }
