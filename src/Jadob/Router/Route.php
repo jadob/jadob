2398a5002cba2d05c93412c88efb1f824e40d17e
pizzaminded <pizzaminded@users.noreply.github.com>
2019-04-25 20:52:56 +0200

    [Router] Path should be required

--- src/Jadob/Router/Route.php
+++ src/Jadob/Router/Route.php
@@ -53,13 +53,13 @@ class Route
 
     /**
      * @param string $name
-     * @param string|null $path
+     * @param string $path
      * @param string|null $controller
      * @param string|null $action
      * @param string|null $host
      * @param array $methods
      */
-    public function __construct($name, $path = null, $controller = null, $action = '__invoke', $host = null, array $methods = [])
+    public function __construct($name, $path, $controller = null, $action = '__invoke', $host = null, array $methods = [])
     {
         $this->name = $name;
         $this->path = $path;
