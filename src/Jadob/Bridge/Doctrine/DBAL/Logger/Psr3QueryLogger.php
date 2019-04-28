60edc4ec57d0de89587338953475d2d70dee38ec
pizzaminded <pizzaminded@users.noreply.github.com>
2019-04-28 18:08:41 +0200

    [DoctrineDBALBridge] refactored bridge
    Signed-off-by: pizzaminded <pizzaminded@users.noreply.github.com>

---
+++ src/Jadob/Bridge/Doctrine/DBAL/Logger/Psr3QueryLogger.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Jadob\Bridge\Doctrine\DBAL\Logger;
+
+use Doctrine\DBAL\Logging\SQLLogger;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Class Psr3QueryLogger
+ * @package Jadob\Bridge\Doctrine\DBAL\Logger
+ * @author pizzaminded <miki@appvende.net>
+ * @license MIT
+ */
+class Psr3QueryLogger implements SQLLogger
+{
+
+    /**
+     * @var LoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * @var array
+     */
+    protected $recentQuery;
+
+    /**
+     * Psr3QueryLogger constructor.
+     * @param LoggerInterface $logger
+     */
+    public function __construct(LoggerInterface $logger)
+    {
+
+        $this->logger = $logger;
+    }
+
+    /**
+     * Logs a SQL statement somewhere.
+     *
+     * @param string $sql The SQL to be executed.
+     * @param mixed[]|null $params The SQL parameters.
+     * @param int[]|string[]|null $types The SQL parameter types.
+     *
+     * @return void
+     */
+    public function startQuery($sql, ?array $params = null, ?array $types = null): void
+    {
+        $this->recentQuery = [
+            'sql' => $sql,
+            'params' => $params,
+            'types' => $types,
+            'start_time' => microtime(true)
+        ];
+    }
+
+    /**
+     * Marks the last started query as stopped. This can be used for timing of queries.
+     *
+     * @return void
+     */
+    public function stopQuery(): void
+    {
+        if (empty($this->recentQuery)) {
+            return;
+        }
+
+        $message = $this->recentQuery['sql'];
+        unset($this->recentQuery['sql']);
+        $this->recentQuery['finish_time'] = microtime(true);
+        $this->logger->info($message, $this->recentQuery);
+        $this->recentQuery = [];
+    }
+}
---
+++ src/Jadob/Bridge/Doctrine/DBAL/ServiceProvider/DoctrineDBALProvider.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Jadob\Bridge\Doctrine\DBAL\ServiceProvider;
+
+use Doctrine\Common\EventManager;
+use Doctrine\DBAL\Configuration;
+use Doctrine\DBAL\DriverManager;
+use Doctrine\DBAL\Tools\Console\Command\ReservedWordsCommand;
+use Doctrine\DBAL\Tools\Console\Command\RunSqlCommand;
+use Jadob\Bridge\Doctrine\DBAL\Logger\Psr3QueryLogger;
+use Jadob\Container\Container;
+use Jadob\Container\ServiceProvider\ServiceProviderInterface;
+use Monolog\Logger;
+use Psr\Container\ContainerInterface;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Helper\HelperSet;
+
+/**
+ * Class DoctrineDBALProvider
+ * @package Jadob\Bridge\Doctrine\DBAL\ServiceProvider
+ * @author pizzaminded <miki@appvende.net>
+ * @license MIT
+ */
+class DoctrineDBALProvider implements ServiceProviderInterface
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigNode()
+    {
+        return 'doctrine_dbal';
+    }
+
+    /**
+     * {@inheritdoc}
+     * @throws \Doctrine\DBAL\DBALException
+     * @throws \RuntimeException
+     */
+    public function register($config)
+    {
+        if (!isset($config['connections']) || \count($config['connections']) === 0) {
+            throw new \RuntimeException('You should provide at least one connection in config.doctrine_dbal node.');
+        }
+
+        $services = [];
+        $services[EventManager::class] = $eventManager = new EventManager();
+
+        $services['doctrine.dbal.logger'] = function (ContainerInterface $container) {
+            $logger = new Logger('doctrine_dbal');
+            $logger->pushHandler($container->get('logger.handler.default'));
+
+            return $logger;
+        };
+
+        $services['doctrine.dbal.query.logger'] = function (ContainerInterface $container) {
+            $logger = new Psr3QueryLogger(
+                $container->get('doctrine.dbal.logger')
+            );
+
+            return $logger;
+        };
+
+        $services[Configuration::class] = function (ContainerInterface $container) {
+            $configuration = new Configuration();
+            $configuration->setSQLLogger($container->get('doctrine.dbal.query.logger'));
+
+            return $configuration;
+        };
+
+        foreach ($config['connections'] as $connectionName => $configuration) {
+            $services['doctrine.dbal.' . $connectionName] = function (ContainerInterface $container) use ($configuration, $eventManager) {
+                return DriverManager::getConnection(
+                    $configuration,
+                    $container->get(Configuration::class),
+                    $eventManager
+                );
+            };
+
+        }
+
+        return $services;
+    }
+
+    /**
+     * {@inheritdoc}
+     * @throws \Jadob\Container\Exception\ServiceNotFoundException
+     * @throws \Symfony\Component\Console\Exception\LogicException
+     */
+    public function onContainerBuild(Container $container, $config)
+    {
+        if($container->has('console')) {
+
+            $helperSet = new HelperSet([
+                'db' => $container->get('doctrine.dbal.default')
+            ]);
+
+            /** @var Application $console */
+            $console = $container->get('console');
+
+            $console->addCommands([
+                new ReservedWordsCommand($helperSet),
+                new RunSqlCommand($helperSet)
+            ]);
+        }
+    }
+}
