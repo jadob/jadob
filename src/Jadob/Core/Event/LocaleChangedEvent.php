77a5007d813cae0e0fe66734b604165ba64c62ed
pizzaminded <pizzaminded@users.noreply.github.com>
2019-08-16 01:25:32 +0200

    [Core] Added new event

---
+++ src/Jadob/Core/Event/LocaleChangedEvent.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Jadob\Core\Event;
+
+/**
+ * @author pizzaminded <miki@appvende.net>
+ * @license MIT
+ */
+class LocaleChangedEvent
+{
+    /**
+     * @var string
+     */
+    protected $newLocale;
+
+    /**
+     * LocaleChangedEvent constructor.
+     * @param string $newLocale
+     */
+    public function __construct(string $newLocale)
+    {
+        $this->newLocale = $newLocale;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNewLocale(): string
+    {
+        return $this->newLocale;
+    }
+}
