3fb8dd9b617ec7b4d012fef4c5390e2cb19f1143
pizzaminded <pizzaminded@users.noreply.github.com>
2019-12-11 00:05:51 +0100

    [MonologBridge] deferredlogger stubs

--- src/Jadob/Bridge/Monolog/DeferredLogger.php
+++ src/Jadob/Bridge/Monolog/DeferredLogger.php
@@ -5,98 +5,35 @@ declare(strict_types=1);
 namespace Jadob\Bridge\Monolog;
 
 use Monolog\Logger;
-use Psr\Log\LoggerInterface;
 
-class DeferredLogger implements LoggerInterface
+class DeferredLogger extends Logger
 {
 
-    protected $logs = [];
-    protected $logger;
+    protected $recordStack = [];
 
-    public function __construct(string $channel)
+
+    /**
+     * {@inheritDoc}
+     */
+    public function addRecord($level, $message, array $context = array())
     {
-        $this->logger = new Logger($channel);
+        //save timestamo
+        //store it
     }
 
-    public function flushLogs()
+    public function close()
     {
 
+        //probably parent::reset() should be called here
+        //Format logs
+        //send to handlers
+        //close handlers
     }
 
     public function __destruct()
     {
-        $this->flushLogs();
+        $this->close();
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    public function emergency($message, array $context = array())
-    {
-        // TODO: Implement emergency() method.
-    }
 
-    /**
-     * {@inheritDoc}
-     */
-    public function alert($message, array $context = array())
-    {
-        // TODO: Implement alert() method.
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function critical($message, array $context = array())
-    {
-        // TODO: Implement critical() method.
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function error($message, array $context = array())
-    {
-        // TODO: Implement error() method.
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function warning($message, array $context = array())
-    {
-        // TODO: Implement warning() method.
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function notice($message, array $context = array())
-    {
-        // TODO: Implement notice() method.
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function info($message, array $context = array())
-    {
-        // TODO: Implement info() method.
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function debug($message, array $context = array())
-    {
-        // TODO: Implement debug() method.
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function log($level, $message, array $context = array())
-    {
-        // TODO: Implement log() method.
-    }
 }
