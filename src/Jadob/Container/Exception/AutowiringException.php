7c5e7f5d3c0f11acef06c18048b0c14d219d5941
pizzaminded <pizzaminded@users.noreply.github.com>
2019-12-12 23:13:31 +0100

    [Container] Added AutowiringException

---
+++ src/Jadob/Container/Exception/AutowiringException.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jadob\Container\Exception;
+
+/**
+ * @author pizzaminded <mikolajczajkowsky@gmail.com>
+ * @license MIT
+ */
+class AutowiringException extends \Exception
+{
+
+}
