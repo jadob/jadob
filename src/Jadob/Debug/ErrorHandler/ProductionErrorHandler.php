ef565f7949eb286bccfb2a3a2f210ea88d93204a
pizzaminded <pizzaminded@users.noreply.github.com>
2019-12-15 21:13:17 +0100

    [Debug] basic production handler

--- src/Jadob/Debug/ErrorHandler/ProductionErrorHandler.php
+++ src/Jadob/Debug/ErrorHandler/ProductionErrorHandler.php
@@ -2,6 +2,8 @@
 
 namespace Jadob\Debug\ErrorHandler;
 
+use Psr\Log\LoggerInterface;
+
 /**
  * @author  pizzaminded <mikolajczajkowsky@gmail.com>
  * @license MIT
@@ -9,13 +11,64 @@ namespace Jadob\Debug\ErrorHandler;
 class ProductionErrorHandler implements ErrorHandlerInterface
 {
 
+    protected $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     public function registerErrorHandler()
     {
-        // TODO: Implement registerErrorHandler() method.
+        $logger = $this->logger;
+
+        if (PHP_SAPI !== 'cli') {
+            set_error_handler(
+                static function ($errno, $errstr, $errfile, $errline) use ($logger) {
+                    if ($errno === E_USER_DEPRECATED || $errno === E_DEPRECATED) {
+                        $message = 'Deprecated: ' . $errstr;
+
+                        /** @noinspection NotOptimalIfConditionsInspection */
+                        if ($errno === E_USER_DEPRECATED) {
+                            $message = 'User Deprecated: ' . $errstr;
+                        }
+                        $context['file'] = $errfile;
+                        $context['line'] = $errline;
+                        $context['stacktrace'] = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+                        $logger->warning($message, $context);
+                        return;
+                    }
+
+                    //According to documentation, it is intended to use error number as a severity
+                    //@see https://www.php.net/manual/en/errorexception.construct.php
+                    throw new \ErrorException($errstr, $errno, $errno, $errfile, $errline);
+                }
+            );
+        }
     }
+
 
     public function registerExceptionHandler()
     {
-        // TODO: Implement registerExceptionHandler() method.
+        $logger = $this->logger;
+        if (PHP_SAPI !== 'cli') {
+            set_exception_handler(
+                static function (\Throwable $exception) use ($logger) {
+                    \http_response_code(500);
+
+                    $logger->critical(
+                        $exception->getMessage(), [
+                            'file' => $exception->getFile(),
+                            'line' => $exception->getLine(),
+                            'trace' => $exception->getTrace(),
+                            'type' => get_class($exception)
+                        ]
+                    );
+
+                    exit;
+
+                }
+            );
+        }
     }
 }
