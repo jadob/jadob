20087f17eda5d845aa7256a3bdfdbec4ba88e29c
pizzaminded <pizzaminded@users.noreply.github.com>
2020-03-01 04:35:55 +0100

    [TwigBridge] fix and refactor test case

--- src/Jadob/Bridge/Twig/Tests/Extension/WebpackManifestAssetExtensionTest.php
+++ src/Jadob/Bridge/Twig/Tests/Extension/WebpackManifestAssetExtensionTest.php
@@ -1,9 +1,12 @@
 <?php
+declare(strict_types=1);
 
 namespace Jadob\Bridge\Twig\Tests\Extension;
 
 use Jadob\Bridge\Twig\Extension\WebpackManifestAssetExtension;
 use PHPUnit\Framework\TestCase;
+use RuntimeException;
+use Twig\TwigFunction;
 
 /**
  * @author  pizzaminded <mikolajczajkowsky@gmail.com>
@@ -15,31 +18,23 @@ class WebpackManifestAssetExtensionTest extends TestCase
     public function testExtensionReturnsTwigFunction()
     {
         $extension = new WebpackManifestAssetExtension([]);
-
         $functions = $extension->getFunctions();
-        /**
- * @var \Twig_SimpleFunction $function 
-*/
+        /** @var TwigFunction $function */
         $function = reset($functions);
 
         $this->assertCount(1, $functions);
-        $this->assertInstanceOf(\Twig_SimpleFunction::class, $function);
+        $this->assertInstanceOf(TwigFunction::class, $function);
 
     }
 
     public function testExtensionCanProperlyReturnValues()
     {
-
         $extension = new WebpackManifestAssetExtension(
-            [
-            'styles.css' => 'styles.1234qwer.css'
-            ]
+            ['styles.css' => 'styles.1234qwer.css']
         );
 
         $functions = $extension->getFunctions();
-        /**
- * @var \Twig_SimpleFunction $function 
-*/
+        /** @var TwigFunction $function */
         $function = reset($functions);
 
         $this->assertEquals('asset_from_manifest', $function->getName());
@@ -47,19 +42,16 @@ class WebpackManifestAssetExtensionTest extends TestCase
         $this->assertEquals('styles.1234qwer.css', $extension->getAssetFromManifest('styles.css'));
     }
 
-    /**
-     * @expectedException        \RuntimeException
-     * @expectedExceptionMessage Could not find "index.js" in webpack manifest file
-     */
+
     public function testExtensionWillBreakWhenNoAssetFound()
     {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Could not find "index.js" in webpack manifest file');
+
         $extension = new WebpackManifestAssetExtension(
-            [
-            'styles.css' => 'styles.2345.css'
-            ]
+            ['styles.css' => 'styles.2345.css']
         );
 
         $extension->getAssetFromManifest('index.js');
-
     }
 }
