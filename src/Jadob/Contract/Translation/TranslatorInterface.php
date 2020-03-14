058fb1ef10a08806679e6b4753f0f9019c740d7c
pizzaminded <pizzaminded@users.noreply.github.com>
2020-03-14 02:13:58 +0100

    [Contract] new component

---
+++ src/Jadob/Contract/Translation/TranslatorInterface.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace Jadob\Contract\Translation;
+
+/**
+ * @author pizzaminded <mikolajczajkowsky@gmail.com>
+ * @license MIT
+ */
+interface TranslatorInterface
+{
+
+    /**
+     * @param string $key
+     * @param array $arguments
+     * @param string|null $group
+     * @return mixed
+     */
+    public function translate(
+        string $key,
+        array $arguments = [],
+        string $group = null
+    ): string;
+
+    /**
+     * @param string $locale
+     */
+    public function setLocale(
+        string $locale
+    ): void;
+
+    /**
+     * @param string $path
+     * @param string $locale
+     * @param string $type
+     * @param string $group
+     * @return mixed
+     */
+    public function addSource(
+        string $path,
+        string $locale,
+        string $type,
+        string $group
+    ): void;
+}
