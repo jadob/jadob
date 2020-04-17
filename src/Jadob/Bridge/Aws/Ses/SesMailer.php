ab6e622d1f18e557061553deaf94256d6ccb5fab
pizzaminded <pizzaminded@users.noreply.github.com>
2020-04-17 23:53:45 +0200

    [AwsBridge] add SesMailer stub

---
+++ src/Jadob/Bridge/Aws/Ses/SesMailer.php
@@ -0,0 +1,71 @@
+<?php
+declare(strict_types=1);
+
+namespace Jadob\Bridge\Aws\Ses;
+
+use Aws\Ses\SesClient;
+use Symfony\Component\Mime\Email;
+
+/**
+ * Sends email using AWS SDK.
+ * @author pizzaminded <mikolajczajkowsky@gmail.com>
+ * @license MIT
+ */
+class SesMailer
+{
+    protected SesClient $sesClient;
+
+    /**
+     * SesMailer constructor.
+     * @param SesClient $sesClient
+     */
+    public function __construct(SesClient $sesClient)
+    {
+        $this->sesClient = $sesClient;
+    }
+
+    /**
+     * @param Email $email
+     */
+    public function send(Email $email)
+    {
+        $charset = 'UTF-8';
+        $plainToEmails = [];
+        foreach ($email->getTo() as $address) {
+            $plainToEmails[] = $address->getAddress();
+        }
+        unset($address);
+
+        $plainReplyToEmails = [];
+        foreach ($email->getReplyTo() as $address) {
+            $plainReplyToEmails[] = $address->getAddress();
+        }
+        unset($address);
+
+        $result = $this->sesClient->sendEmail([
+            'Destination' => [
+                'ToAddresses' => $plainToEmails,
+            ],
+            'ReplyToAddresses' => $plainReplyToEmails,
+            'Source' => $email->getFrom(),
+            'Message' => [
+                'Body' => [
+                    'Html' => [
+                        'Charset' => $email->getHtmlCharset(),
+                        'Data' => $email->getHtmlBody(),
+                    ],
+                    'Text' => [
+                        'Charset' => $email->getTextCharset(),
+                        'Data' => $email->getTextBody(),
+                    ],
+                ],
+                'Subject' => [
+                    'Charset' => $charset,
+                    'Data' => $email->getSubject(),
+                ],
+            ]
+        ]);
+
+
+    }
+}
