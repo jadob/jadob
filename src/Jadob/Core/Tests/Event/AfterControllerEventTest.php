eadd56e12104c967dd4a28cef86279799946c3a1
pizzaminded <pizzaminded@users.noreply.github.com>
2020-08-29 04:37:14 +0200

    chore: fix tests

--- src/Jadob/Core/Tests/Event/AfterControllerEventTest.php
+++ src/Jadob/Core/Tests/Event/AfterControllerEventTest.php
@@ -5,8 +5,10 @@ namespace Jadob\Core\Tests\Event;
 
 
 use Jadob\Core\Event\AfterControllerEvent;
+use Jadob\Core\RequestContext;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class AfterControllerEventTest extends TestCase
@@ -14,8 +16,9 @@ class AfterControllerEventTest extends TestCase
 
     public function testResponseOverriding()
     {
+        $context = new RequestContext('test', Request::createFromGlobals(), false);
         $orginalResponse = new Response();
-        $event = new AfterControllerEvent($orginalResponse);
+        $event = new AfterControllerEvent($orginalResponse, $context);
 
         $this->assertSame($orginalResponse, $event->getResponse());
 
