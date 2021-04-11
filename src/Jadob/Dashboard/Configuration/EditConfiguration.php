07fa031391da6378340420f51ca637ed5282585b
pizzaminded <pizzaminded@users.noreply.github.com>
2021-04-11 03:02:30 +0200

    feat(dashboard): add EditConfiguration.php

---
+++ src/Jadob/Dashboard/Configuration/EditConfiguration.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace Jadob\Dashboard\Configuration;
+
+
+use Jadob\Dashboard\Exception\ConfigurationException;
+
+class EditConfiguration
+{
+
+    protected bool $inheritNewForm = false;
+
+    /**
+     * @param array $data
+     * @return static
+     * @throws ConfigurationException
+     */
+    public static function fromArray(array $data): static
+    {
+        if(
+            !isset($data['inherit_new_form'])
+            && !is_bool($data['inherit_new_form'])
+        ) {
+            throw new ConfigurationException('Invalid Edit Form configuration passed.');
+        }
+
+        $self = new self();
+        $self->inheritNewForm = $data['inherit_new_form'];
+
+        return $self;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isInheritNewForm(): bool
+    {
+        return $this->inheritNewForm;
+    }
+
+}
