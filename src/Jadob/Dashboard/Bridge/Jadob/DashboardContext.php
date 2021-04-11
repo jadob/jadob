ee9e52548d1cb270387c56af68c6b0bed57fccec
pizzaminded <pizzaminded@users.noreply.github.com>
2021-04-11 03:34:52 +0200

    fix(dashboard): assign user to property

--- src/Jadob/Dashboard/Bridge/Jadob/DashboardContext.php
+++ src/Jadob/Dashboard/Bridge/Jadob/DashboardContext.php
@@ -3,23 +3,24 @@ declare(strict_types=1);
 
 namespace Jadob\Dashboard\Bridge\Jadob;
 
-
+use DateTimeInterface;
 use Jadob\Contracts\Dashboard\DashboardContextInterface;
 use Jadob\Security\Auth\User\UserInterface;
 
 class DashboardContext implements DashboardContextInterface
 {
 
-    protected \DateTimeInterface $requestDateTime;
+    protected DateTimeInterface $requestDateTime;
 
     protected UserInterface $user;
 
-    public function __construct(\DateTimeInterface $requestDateTime, UserInterface $user)
+    public function __construct(DateTimeInterface $requestDateTime, UserInterface $user)
     {
         $this->requestDateTime = $requestDateTime;
+        $this->user = $user;
     }
 
-    public function getRequestDateTime(): \DateTimeInterface
+    public function getRequestDateTime(): DateTimeInterface
     {
         return $this->requestDateTime;
     }
