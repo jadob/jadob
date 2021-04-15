c5ab50a414b5f6e7491d9441eb3b3e32656de361
pizzaminded <pizzaminded@users.noreply.github.com>
2021-04-15 20:55:31 +0200

    chore(core): bump version

--- src/Jadob/Core/Kernel.php
+++ src/Jadob/Core/Kernel.php
@@ -54,7 +54,7 @@ class Kernel
      * @see https://semver.org/
      * @var string
      */
-    public const VERSION = '0.1.6';
+    public const VERSION = '0.2.0';
 
     /**
      * If true, application log will be saved while destructing objects
