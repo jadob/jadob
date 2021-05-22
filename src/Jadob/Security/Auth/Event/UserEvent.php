94b6629458b3f90bda60ecd23c09c0fceae36b7c
pizzaminded <pizzaminded@users.noreply.github.com>
2021-05-22 04:20:32 +0200

    feat(Auth): Add UserEvent.php

---
+++ src/Jadob/Security/Auth/Event/UserEvent.php
@@ -0,0 +1,79 @@
+<?php
+declare(strict_types=1);
+
+namespace Jadob\Security\Auth\Event;
+
+
+use Jadob\Security\Auth\User\UserInterface;
+use Jadob\Security\Supervisor\RequestSupervisor\RequestSupervisorInterface;
+
+class UserEvent
+{
+    /**
+     * User was just authenticated.
+     */
+    public const CONTEXT_AUTHENTICATED = 1;
+
+    /**
+     * User was authenticated few requests ago, and it is just taken from IdentityStorage.
+     * Refresh or something may be required.
+     */
+    public const CONTEXT_TAKEN_FROM_SESSION = 2;
+
+    protected UserInterface $user;
+    protected int $context;
+    protected RequestSupervisorInterface $requestSupervisor;
+
+    public function __construct(
+        UserInterface $user,
+        int $context,
+        RequestSupervisorInterface $requestSupervisor
+    )
+    {
+        $this->user = $user;
+        $this->context = $context;
+        $this->requestSupervisor = $requestSupervisor;
+    }
+
+    public function isAuthenticated(): bool
+    {
+        return $this->context === self::CONTEXT_AUTHENTICATED;
+    }
+
+    public function isTakenFromIdentityStorage(): bool
+    {
+        return $this->context === self::CONTEXT_TAKEN_FROM_SESSION;
+    }
+
+    /**
+     * @return UserInterface
+     */
+    public function getUser(): UserInterface
+    {
+        return $this->user;
+    }
+
+    /**
+     * @return int
+     */
+    public function getContext(): int
+    {
+        return $this->context;
+    }
+
+    /**
+     * @param UserInterface $user
+     */
+    public function setUser(UserInterface $user): void
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @return RequestSupervisorInterface
+     */
+    public function getRequestSupervisor(): RequestSupervisorInterface
+    {
+        return $this->requestSupervisor;
+    }
+}
