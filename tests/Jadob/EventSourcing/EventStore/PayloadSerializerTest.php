719485c2b5d49305ed7a4ed234f03da440fd057e
pizzaminded <pizzaminded@users.noreply.github.com>
2021-06-04 13:43:11 +0200

    refactor: moving test cases to separate dir

--- tests/Jadob/EventSourcing/EventStore/PayloadSerializerTest.php
+++ tests/Jadob/EventSourcing/EventStore/PayloadSerializerTest.php
@@ -1,9 +1,8 @@
 <?php
 declare(strict_types=1);
 
-namespace Jadob\EventSourcing\Tests\EventStore;
+namespace Jadob\EventSourcing\EventStore;
 
-use Jadob\EventSourcing\EventStore\PayloadSerializer;
 use Monolog\Test\TestCase;
 
 /**
