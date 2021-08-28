0ad8a5b4961b48f26dbd15f975929ccf1f4ccaab
pizzaminded <pizzaminded@users.noreply.github.com>
2021-08-29 00:25:48 +0200

    feat(event-sourcing): add RepositoryFactory

---
+++ src/Jadob/EventSourcing/Aggregate/RepositoryFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace Jadob\EventSourcing\Aggregate;
+
+
+use Jadob\EventSourcing\EventStore\EventStoreInterface;
+use Jadob\EventSourcing\EventStore\PayloadSerializer;
+
+class RepositoryFactory
+{
+    private EventStoreInterface $eventStore;
+    private PayloadSerializer $serializer;
+
+    public function __construct(
+        EventStoreInterface $eventStore,
+        PayloadSerializer $serializer
+    )
+    {
+        $this->eventStore = $eventStore;
+        $this->serializer = $serializer;
+    }
+
+    /**
+     * @param string $aggregateType Aggregate Root FQCN
+     * @return AggregateRepository
+     */
+    public function create(string $aggregateType): AggregateRepository
+    {
+        return new AggregateRepository(
+            $this->eventStore,
+            $this->serializer,
+            $aggregateType
+        );
+    }
+}
