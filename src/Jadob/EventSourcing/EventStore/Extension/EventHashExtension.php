ae01871000e9d36f04c3b0fec60000348df846d4
pizzaminded <pizzaminded@users.noreply.github.com>
2021-08-29 00:17:55 +0200

    feat(event-sourcing): add EventHashExtension

---
+++ src/Jadob/EventSourcing/EventStore/Extension/EventHashExtension.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace Jadob\EventSourcing\EventStore\Extension;
+
+
+use Jadob\EventSourcing\Aggregate\AggregateRootInterface;
+use Jadob\EventSourcing\Aggregate\DomainEventInterface;
+use Jadob\EventSourcing\EventStore\AggregateMetadata;
+use Jadob\EventSourcing\EventStore\EventStoreExtensionInterface;
+
+class EventHashExtension implements EventStoreExtensionInterface
+{
+
+    public const ATTRIBUTE = 'hash';
+
+    /**
+     * @inheritDoc
+     */
+    public function onAggregateCreate(AggregateRootInterface $aggregate, AggregateMetadata $metadata): void
+    {
+        // TODO: Implement onAggregateCreate() method.
+    }
+
+
+    /**
+     * @inheritDoc
+     */
+    public function onEventAppend(DomainEventInterface $event, string $payload, AggregateRootInterface $aggregate): void
+    {
+        $event->addAttribute(
+            self::ATTRIBUTE,
+            $this->calculateEventHash(
+                $payload,
+                $aggregate->getAggregateId(),
+                $event->getEventId()
+            )
+        );
+    }
+
+    public function calculateEventHash(string $payload, string $aggregateId, string $eventId): string
+    {
+        return hash('sha256', sprintf('%s.%s.%s', $payload, $aggregateId, $eventId));
+    }
+}
