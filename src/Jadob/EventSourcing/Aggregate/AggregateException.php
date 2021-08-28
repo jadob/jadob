e81a129feda972c456794c263e94c7336422b6bf
pizzaminded <pizzaminded@users.noreply.github.com>
2021-08-28 23:43:47 +0200

    feat(event-sourcing): fix CS

--- src/Jadob/EventSourcing/Aggregate/AggregateException.php
+++ src/Jadob/EventSourcing/Aggregate/AggregateException.php
@@ -1,13 +1,10 @@
 <?php
 
-declare(strict_types=1);
 
 namespace Jadob\EventSourcing\Aggregate;
 
-/**
- * @author pizzaminded <mikolajczajkowsky@gmail.com>
- */
-class AggregateException extends \Error
+
+class AggregateException extends \Exception
 {
 
 }
