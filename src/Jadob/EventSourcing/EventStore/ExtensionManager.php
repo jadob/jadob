309dd31963eb0022e44c64e17fafdf6b117f313b
pizzaminded <pizzaminded@users.noreply.github.com>
2021-08-29 00:18:07 +0200

    feat(event-sourcing): add ExtensionManager

---
+++ src/Jadob/EventSourcing/EventStore/ExtensionManager.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace Jadob\EventSourcing\EventStore;
+
+
+use Jadob\EventSourcing\Aggregate\AggregateRootInterface;
+use Jadob\EventSourcing\Aggregate\DomainEventInterface;
+
+/**
+ * @author pizzaminded <mikolajczajkowsky@gmail.com>
+ * @license proprietary
+ * @internal
+ */
+class ExtensionManager
+{
+
+    /**
+     * @var EventStoreExtensionInterface[]
+     */
+    private array $extensions;
+
+    /**
+     * @param EventStoreExtensionInterface[] $extensions
+     */
+    public function __construct(array $extensions)
+    {
+        $this->extensions = $extensions;
+
+    }
+
+    public function dispatchOnAggregateCreate(AggregateRootInterface $aggregate, AggregateMetadata $metadata)
+    {
+        foreach ($this->extensions as $extension) {
+            $extension->onAggregateCreate($aggregate, $metadata);
+        }
+    }
+
+    public function dispatchOnEventAppend(DomainEventInterface $event, string $payload, AggregateRootInterface $aggregateRoot)
+    {
+        foreach ($this->extensions as $extension) {
+            $extension->onEventAppend($event,$payload, $aggregateRoot);
+        }
+    }
+}
