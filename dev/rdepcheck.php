8b12fad9f3a08dda5259aa7c56a250897c936c08
pizzaminded <pizzaminded@users.noreply.github.com>
2021-08-29 02:03:11 +0200

    chore: add replace section

--- dev/rdepcheck.php
+++ dev/rdepcheck.php
@@ -4,10 +4,30 @@
  */
 
 
-function println(string $line): void {
-    echo $line.PHP_EOL;
+function println(string $line): void
+{
+    echo $line . PHP_EOL;
 }
 
+function getJsonContents(string $file): array
+{
+    return json_decode(file_get_contents($file), true, 512, JSON_THROW_ON_ERROR);
+}
+
+function getNestedJsonPackages(string $path): array
+{
+    $packagesFound = [];
+    $dirIterator = new RecursiveDirectoryIterator($path);
+    $iterator = new RecursiveIteratorIterator($dirIterator);
+    foreach ($iterator as $file) {
+        /** @var SplFileInfo $file */
+        if ($file->getFilename() === 'composer.json') {
+            $packagesFound[] = $file->getPathname();
+        }
+    }
+
+    return $packagesFound;
+}
 
 println('Im going to check your composer.json files!');
 
@@ -15,21 +35,17 @@ println('Im going to check your composer.json files!');
 $cwd = getcwd();
 
 
-println('Scanning src dir...');
-$dirIterator = new RecursiveDirectoryIterator(sprintf('%s/src', $cwd));
-$iterator = new RecursiveIteratorIterator($dirIterator);
+println('Scanning src dir.');
 
-$packagesFound = [];
+$nestedPackages = [];
+
+$coreComposerFile = sprintf('%s/composer.json', $cwd);
+$packagesFound = getNestedJsonPackages(sprintf('%s/src', $cwd));
+
 $errors = [];
 
-foreach ($iterator as $file) {
-    /** @var SplFileInfo $file */
-    if($file->getFilename() === 'composer.json') {
-        $packagesFound[] = $file->getPathname();
-    }
-}
 
-if(count($packagesFound) === 0) {
+if (count($packagesFound) === 0) {
     println('Found nothing.');
     die(0);
 }
@@ -37,32 +53,56 @@ if(count($packagesFound) === 0) {
 
 $requiredKey = ['name', 'license', 'description', 'autoload'];
 
-println('Processing files...');
+println('Checking for basic composer.json issues.');
 foreach ($packagesFound as $package) {
     try {
-        $composerFile = json_decode(file_get_contents($package), true, 512,  JSON_THROW_ON_ERROR);
+        $composerFile = getJsonContents($package);
 
-        foreach ($requiredKey  as $key) {
-            if(!isset($composerFile[$key])) {
+        foreach ($requiredKey as $key) {
+            if (!isset($composerFile[$key])) {
                 $errors[] = sprintf('%s: missing "%s" key', $package, $key);
                 continue 2;
             }
         }
 
-        if($composerFile['license'] !== 'MIT') {
-            $errors[] = sprintf('%s: invalid license (should be MIT, uppercased)', $package, $key);
+        if ($composerFile['license'] !== 'MIT') {
+            $errors[] = sprintf('%s: invalid license (should be MIT, uppercased)', $package);
         }
 
 
-
+        $nestedPackages[$coreComposerFile][] = $package;
+        $nestedPackagesForPackage = getNestedJsonPackages(dirname($package));
+        foreach ($nestedPackagesForPackage as $key => $value) {
+            if($value === $package) {
+                unset($nestedPackagesForPackage[$key]);
+            }
+        }
+        if(count($nestedPackagesForPackage) > 0) {
+            $nestedPackages[$package] = $nestedPackagesForPackage;
+        }
     } catch (JsonException $exception) {
         $errors[] = sprintf('Unable to parse %s: %s', $package, $exception->getMessage());
     }
 }
 
+println('Checking for nesting replaces.');
 
 
-if(count($errors) > 0) {
+foreach ($nestedPackages as $packagePath => $nestedPackagePaths) {
+    $corePackage = getJsonContents($packagePath);
+    foreach ($nestedPackagePaths as $nestedPackagePath) {
+        $nestedPackage = getJsonContents($nestedPackagePath);
+        $nestedPackageName = $nestedPackage['name'];
+
+
+        if(!isset($corePackage['replace'][$nestedPackageName])) {
+            $errors[] = sprintf('%s: missing replace.%s ', $packagePath, $nestedPackageName);
+        }
+    }
+
+}
+
+if (count($errors) > 0) {
     println('Got Errors:');
 
     foreach ($errors as $error) {
@@ -72,6 +112,6 @@ if(count($errors) > 0) {
     die(1);
 }
 
-println('Done');
+println('Nice, no errors found!');
 die(0);
 
