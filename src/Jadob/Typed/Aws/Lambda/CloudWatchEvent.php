d651eb22ef4d5bac22ffb26ba1ca495c1aa93e1e
pizzaminded <pizzaminded@users.noreply.github.com>
2021-09-17 16:53:28 +0200

    feat(typed): first types for AWS Lambda

---
+++ src/Jadob/Typed/Aws/Lambda/CloudWatchEvent.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Jadob\Typed\Aws\Lambda;
+
+
+/**
+ * @see https://docs.aws.amazon.com/lambda/latest/dg/services-cloudwatchevents.html
+ * @see https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/aws-lambda/trigger/eventbridge.d.ts
+ * @author pizzaminded <mikolajczajkowsky@gmail.com>
+ * @license MIT
+ */
+final class CloudWatchEvent extends EventBridgeEvent
+{
+
+}
---
+++ src/Jadob/Typed/Aws/Lambda/EventBridgeEvent.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace Jadob\Typed\Aws\Lambda;
+
+
+/**
+ * @see https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/aws-lambda/trigger/eventbridge.d.ts
+ * @author pizzaminded <mikolajczajkowsky@gmail.com>
+ */
+class EventBridgeEvent
+{
+    protected string $id;
+    protected string $version;
+    protected string $account;
+    protected string $time;
+    protected string $region;
+    /**
+     * @var string[]
+     */
+    protected array $resources;
+    protected string $source;
+    /**
+     * @var string
+     */
+    protected string $detailType; // detail-type
+    /**
+     * @var array
+     */
+    protected array $detail;
+
+}
