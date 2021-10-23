42240354452b6a0ec9580772cba13b00a47040e4
pizzaminded <pizzaminded@users.noreply.github.com>
2021-10-23 03:06:58 +0200

    feat: add DashboardConfigurationExtension.php

---
+++ src/Jadob/Dashboard/Twig/DashboardConfigurationExtension.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace Jadob\Dashboard\Twig;
+
+
+use Jadob\Dashboard\Configuration\DashboardConfiguration;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class DashboardConfigurationExtension extends AbstractExtension
+{
+    protected DashboardConfiguration $configuration;
+
+    public function __construct(
+        DashboardConfiguration $configuration
+    )
+    {
+        $this->configuration = $configuration;
+    }
+
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('dashboard_get_config', [$this, 'getDashboardConfig'])
+        ];
+    }
+
+    public function getDashboardConfig(): DashboardConfiguration
+    {
+        return $this->configuration;
+    }
+
+}
