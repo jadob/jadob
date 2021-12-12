9def0756807368704665d4f010b0ffbcd19afa4d
pizzaminded <pizzaminded@users.noreply.github.com>
2021-12-12 04:47:54 +0100

    feat(contracts, container): add InjectParam

---
+++ src/Jadob/Contracts/Container/Attribute/InjectParam.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace Jadob\Contracts\Container\Attribute;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_PROPERTY)]
+class InjectParam
+{
+    public function __construct(
+        protected string $name
+    )
+    {
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+}
