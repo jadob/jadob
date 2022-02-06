ec1e4bd90c4b504a8a94d2c91cf1b92c63785438
pizzaminded <pizzaminded@users.noreply.github.com>
2022-02-06 03:29:07 +0100

    v0.4.1

--- src/Jadob/Core/Kernel.php
+++ src/Jadob/Core/Kernel.php
@@ -56,7 +56,7 @@ class Kernel
      * @see https://semver.org/
      * @var string
      */
-    public const VERSION = '0.4.0';
+    public const VERSION = '0.4.1';
 
     /**
      * If true, application log will be saved while destructing objects
