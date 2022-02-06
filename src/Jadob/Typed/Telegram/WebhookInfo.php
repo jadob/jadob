bf1acd3d6d0d3116dc22974b38b48ea3c33e618e
pizzaminded <pizzaminded@users.noreply.github.com>
2022-02-06 01:35:28 +0100

    fix(typed-telegram): add WebhookInfo.php

---
+++ src/Jadob/Typed/Telegram/WebhookInfo.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace Jadob\Typed\Telegram;
+
+
+class WebhookInfo
+{
+    protected ?string $url = null;
+
+
+    public static function fromArray(array $data): self
+    {
+        $self = new self();
+        $self->url = $data['url'] ?? null;
+
+        return $self;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    /**
+     * @param string|null $url
+     * @return WebhookInfo
+     */
+    public function setUrl(?string $url): WebhookInfo
+    {
+        $this->url = $url;
+        return $this;
+    }
+
+}
