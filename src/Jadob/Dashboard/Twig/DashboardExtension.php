5609c4cffba9ffc83c980845dc032d8c1022646b
pizzaminded <pizzaminded@users.noreply.github.com>
2022-02-13 23:50:59 +0100

    feat(dashboard): add some colors to show template

--- src/Jadob/Dashboard/Twig/DashboardExtension.php
+++ src/Jadob/Dashboard/Twig/DashboardExtension.php
@@ -21,9 +21,22 @@ class DashboardExtension extends AbstractExtension
     public function getFunctions()
     {
         return [
+            new TwigFunction('dashboard_val_is_true', [$this, 'isTrue']),
+            new TwigFunction('dashboard_val_is_false', [$this, 'isFalse']),
+            new TwigFunction('dashboard_get_component_data', [$this, 'getComponentData']),
             new TwigFunction('dashboard_get_component_data', [$this, 'getComponentData']),
             new TwigFunction('dashboard_get_template', [$this, 'getTemplate'])
         ];
+    }
+
+    public function isTrue($val): bool
+    {
+        return $val === true;
+    }
+
+    public function isFalse($val): bool
+    {
+        return $val === false;
     }
 
     public function getComponentData(
