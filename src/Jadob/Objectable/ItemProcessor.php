12de1124fefe33d7eb6c5c4d3c7f0dd9fb5867c2
pizzaminded <pizzaminded@users.noreply.github.com>
2022-02-13 20:01:50 +0100

    feat(objectable): extraction from Objectable class in progress

--- src/Jadob/Objectable/ItemProcessor.php
+++ src/Jadob/Objectable/ItemProcessor.php
@@ -5,6 +5,7 @@ namespace Jadob\Objectable;
 
 
 use Doctrine\Common\Annotations\Reader;
+use Jadob\Objectable\Annotation\Field;
 use Symfony\Component\PropertyAccess\PropertyAccess;
 use Symfony\Component\PropertyAccess\PropertyAccessor;
 
@@ -18,24 +19,51 @@ class ItemProcessor
     )
     {
 
-        if($this->propertyAccessor === null) {
-           $this->propertyAccessor = PropertyAccess::createPropertyAccessor();
+        if ($this->propertyAccessor === null) {
+            $this->propertyAccessor = PropertyAccess::createPropertyAccessor();
         }
 
-        if($this->metadataParser === null) {
+        if ($this->metadataParser === null) {
             $this->metadataParser = new ItemMetadataParser();
         }
     }
 
     /**
-     * Returns array of values from fields annotated with Header class and matching context.
+     * Returns array of values from fields annotated with Field class and matching context.
      * @param object $item
      * @param string[] $context
      * @return array
      */
     public function extractItemValues(object $item, array $context = ['default']): array
     {
-        $metadata = $this->metadataParser->parse(get_class($item));
+        $output = [];
+        $ref = new \ReflectionClass($item);
+        $props = $ref->getProperties();
 
+        foreach ($props as $reflectionProperty) {
+            $attrs = $reflectionProperty->getAttributes(Field::class);
+            $shouldExtract = false;
+            foreach ($attrs as $fieldAttr) {
+                /** @var Field $instance */
+                $instance = $fieldAttr->newInstance();
+
+                #TODO: there should be some array intersection or something similar
+                foreach ($context as $singleContext) {
+                    if ($instance->hasContext($singleContext)) {
+                        $val = $reflectionProperty->getValue($item);
+
+                        $output[$instance->getName()] = $val;
+
+                        if ($instance->isStringable()) {
+                            $output[$instance->getName()] = (string)$val;
+                        }
+                    }
+                }
+            }
+
+
+        }
+
+        return $output;
     }
 }
