4ec7c5137a6e2454830ad19ba3d0dfeae9137564
pizzaminded <pizzaminded@users.noreply.github.com>
2022-02-14 02:32:07 +0100

    feat(webhook-handler, webhook-provider-telegram): event extracting

--- src/Jadob/Contracts/Webhook/EventExtractorInterface.php
+++ src/Jadob/Contracts/Webhook/EventExtractorInterface.php
@@ -1,9 +1,17 @@
 <?php
-declare(strict_types=1);
 
+declare(strict_types=1);
 
 namespace Jadob\Contracts\Webhook;
 
+use Symfony\Component\HttpFoundation\Request;
+
 interface EventExtractorInterface
 {
+    /**
+     * Checks if request contains any legitimate payload that can be later extracted into an event class.
+     */
+    public function canProcess(Request $request): bool;
+
+    public function extractEvent(Request $request): object;
 }
--- src/Jadob/Webhook/Provider/Telegram/TelegramEventExtractor.php
+++ src/Jadob/Webhook/Provider/Telegram/TelegramEventExtractor.php
@@ -4,8 +4,20 @@ declare(strict_types=1);
 namespace Jadob\Webhook\Provider\Telegram;
 
 use Jadob\Contracts\Webhook\EventExtractorInterface;
+use Jadob\Typed\Telegram\Update;
+use Symfony\Component\HttpFoundation\Request;
 
 class TelegramEventExtractor implements EventExtractorInterface
 {
 
+    public function canProcess(Request $request): bool
+    {
+        $payload = $request->toArray();
+        return isset($payload['update_id']);
+    }
+
+    public function extractEvent(Request $request): object
+    {
+        return Update::fromArray($request->toArray());
+    }
 }
