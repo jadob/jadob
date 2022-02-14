8607dcc3badbc3902ac2aa738cf7f824b8845b91
pizzaminded <pizzaminded@users.noreply.github.com>
2022-02-14 02:32:26 +0100

    feat(webhook-handler): handler in progress

--- src/Jadob/Webhook/Handler/Service/RequestHandler.php
+++ src/Jadob/Webhook/Handler/Service/RequestHandler.php
@@ -4,17 +4,45 @@ declare(strict_types=1);
 namespace Jadob\Webhook\Handler\Service;
 
 use Psr\EventDispatcher\EventDispatcherInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
 
 class RequestHandler
 {
+    protected const CHANNELS_REQUEST_ATTR = 'webhook_channels';
+
     public function __construct(
-        protected EventDispatcherInterface $eventDispatcher
+        protected EventDispatcherInterface $eventDispatcher,
+        protected ProviderRegistry $providerRegistry,
+        protected LoggerInterface $logger
     ) {
     }
 
 
     public function handle(Request $request)
     {
+        $channels = $request->attributes->get(self::CHANNELS_REQUEST_ATTR);
+
+        $this->logger->info('New webhook request had arrived!', [
+            'payload' => $request->getContent(),
+            'headers' => $request->headers->all()
+        ]);
+
+        foreach ($channels as $channel) {
+            $provider = $this->providerRegistry->getProvider($channel);
+            $requestValidator  = $provider->getRequestValidator();
+            $extractor = $provider->getEventExtractor();
+            
+            if($requestValidator !== null) {
+                $requestValidator->validate($request);
+            }
+
+            if(!$extractor->canProcess($request)) {
+                #TODO: continue to another provider
+            }
+
+            $event = $extractor->extractEvent($request);
+
+        }
     }
 }
