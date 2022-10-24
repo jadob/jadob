42c3680f5645cc86e8710766a037c5fd8bc7fc6c
pizzaminded <pizzaminded@users.noreply.github.com>
2022-10-24 21:14:00 +0200

    v0.4.13

--- src/Jadob/Core/Kernel.php
+++ src/Jadob/Core/Kernel.php
@@ -56,7 +56,7 @@ class Kernel
      * @see https://semver.org/
      * @var string
      */
-    public const VERSION = '0.4.12';
+    public const VERSION = '0.4.13';
 
     /**
      * @var Config
