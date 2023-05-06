7c2f3984fffcdbaa74c4f0e3d837960c30a78c2c
pizzaminded <pizzaminded@users.noreply.github.com>
2023-05-06 23:40:34 +0200

    refactor(container): remove not-so-useful tests

--- tests/unit/Jadob/Container/DefinitionTest.php
+++ tests/unit/Jadob/Container/DefinitionTest.php
@@ -3,9 +3,7 @@
 namespace Jadob\Container;
 
 use Jadob\Container\Fixtures\ExampleService;
-use Jadob\Container\Fixtures\YetAnotherExampleService;
 use PHPUnit\Framework\TestCase;
-use Psr\Container\ContainerInterface;
 
 /**
  * @author  pizzaminded <mikolajczajkowsky@gmail.com>
@@ -13,49 +11,6 @@ use Psr\Container\ContainerInterface;
  */
 class DefinitionTest extends TestCase
 {
-
-    public function testServiceCreating(): void
-    {
-        $container = new Container();
-        $container->add(ExampleService::class, $example = new ExampleService());
-
-
-        $factory = function (ContainerInterface $container): YetAnotherExampleService {
-            return new YetAnotherExampleService();
-        };
-
-        $definition = new Definition($factory);
-
-        $definition->addMethodCall('setService', [$example]);
-        /**
- * @var YetAnotherExampleService $createdService 
-*/
-        $createdService = $definition->create($container);
-
-        $this->assertInstanceOf(YetAnotherExampleService::class, $createdService);
-        $this->assertSame($example, $createdService->getService());
-
-        $createdService = $definition->create($container);
-
-        $this->assertInstanceOf(YetAnotherExampleService::class, $createdService);
-        $this->assertSame($example, $createdService->getService());
-
-    }
-
-    public function testScalarCreating(): void
-    {
-        $container = new Container();
-        $container->add(ExampleService::class, $example = new ExampleService());
-
-        $definition = new Definition('hello');
-
-
-        $createdService = $definition->create($container);
-
-        $this->assertEquals('hello', $createdService);
-
-
-    }
 
     public function testTagging()
     {
