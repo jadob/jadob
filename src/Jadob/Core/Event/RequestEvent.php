f6bc7e58c125640ff2e47ce5dcde7d420b1d3ab9
pizzaminded <pizzaminded@users.noreply.github.com>
2024-06-15 02:33:01 +0200

    feat(core): create RequestEvent.php

---
+++ src/Jadob/Core/Event/RequestEvent.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jadob\Core\Event;
+
+use Jadob\Core\RequestContext;
+use Psr\EventDispatcher\StoppableEventInterface;
+use Symfony\Component\HttpFoundation\Response;
+
+class RequestEvent implements StoppableEventInterface
+{
+    /**
+     * @var Response|null
+     */
+    protected ?Response $response = null;
+
+    public function __construct(
+        protected RequestContext $requestContext
+    )
+    {
+    }
+
+    public function getRequestContext(): RequestContext
+    {
+        return $this->requestContext;
+    }
+
+    public function getResponse(): ?Response
+    {
+        return $this->response;
+    }
+
+    public function setResponse(?Response $response): void
+    {
+        $this->response = $response;
+    }
+
+    public function isPropagationStopped(): bool
+    {
+        return $this->response !== null;
+    }
+}
