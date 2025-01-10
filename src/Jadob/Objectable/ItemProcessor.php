ad2618cdf45fefdeb24cf1c90a2d94abc8a0fdfb
pizzaminded <pizzaminded@users.noreply.github.com>
2025-01-10 22:28:23 +0100

    refactor(objectable): allow for only one context when processing

--- src/Jadob/Objectable/ItemProcessor.php
+++ src/Jadob/Objectable/ItemProcessor.php
@@ -18,7 +18,7 @@ class ItemProcessor
      *
      * @param string[] $context
      */
-    public function extractItemValues(object $item, array $context = ['default']): array
+    public function extractItemValues(object $item, string $context = 'default'): array
     {
         $output = [];
         $ref = new ReflectionClass($item);
@@ -31,20 +31,19 @@ class ItemProcessor
                 /** @var Field $instance */
                 $instance = $fieldAttr->newInstance();
 
-                foreach ($context as $singleContext) {
-                    if ($instance->hasContext($singleContext)) {
-                        $val = $reflectionProperty->getValue($item);
+                if ($instance->hasContext($context)) {
+                    $val = $reflectionProperty->getValue($item);
 
-                        if($instance->getMethod() !== null) {
-                            $methodName = $instance->getMethod();
-                            $val = $item->$methodName();
-                        }
+                    if ($instance->getMethod() !== null) {
+                        $methodName = $instance->getMethod();
+                        $val = $item->$methodName();
+                    }
 
-                        $output[$instance->getName()] = $val;
+                    $output[$instance->getName()] = $val;
 
-                        foreach ($translations as $translationReflection) {
-                            /** @var Translate $translationAttr */
-                            $translationAttr = $translationReflection->newInstance();
+                    foreach ($translations as $translationReflection) {
+                        /** @var Translate $translationAttr */
+                        $translationAttr = $translationReflection->newInstance();
 
                             if ($val === $translationAttr->getWhen()) {
                                 $output[$instance->getName()] = $translationAttr->getThen();
@@ -52,63 +51,62 @@ class ItemProcessor
                             }
                         }
 
-                        if ($val instanceof DateTimeInterface) {
-                            $dateFormat = $instance->getDateFormat();
-                            if ($dateFormat === null) {
-                                throw new LogicException('Could not process DateTime object as there is no dateFormat passed in Field.');
-                            }
+                    if ($val instanceof DateTimeInterface) {
+                        $dateFormat = $instance->getDateFormat();
+                        if ($dateFormat === null) {
+                            throw new LogicException('Could not process DateTime object as there is no dateFormat passed in Field.');
+                        }
 
-                            $output[$instance->getName()] = $val->format($dateFormat);
+                        $output[$instance->getName()] = $val->format($dateFormat);
+                        continue;
+                    }
+
+                    /**
+                     * Enables support for doctrine/orm entities, or any other lib that utilises doctrine/collections.
+                     */
+                    if ($val instanceof Collection) {
+                        $collectionItems = $val->toArray();
+                        $processedCollectionItems = [];
+                        foreach ($collectionItems as $collectionItem) {
+                            $processedCollectionItems[] = $this->extractItemValues($collectionItem, $context);
+                        }
+
+                        $output[$instance->getName()] = $processedCollectionItems;
+                    }
+
+
+                    if ($instance->isStringable()) {
+                        $output[$instance->getName()] = (string)$val;
+                        continue;
+                    }
+
+                    if ($instance->isFlat()) {
+                        if ($val === null) {
+                            $output[$instance->getName()] = null;
                             continue;
                         }
 
-                        /**
-                         * Enables support for doctrine/orm entities, or any other lib that utilises doctrine/collections.
-                         */
-                        if ($val instanceof Collection) {
-                            $collectionItems = $val->toArray();
-                            $processedCollectionItems = [];
-                            foreach ($collectionItems as $collectionItem) {
-                                $processedCollectionItems[] = $this->extractItemValues($collectionItem, $context);
-                            }
+                        $flattenedVal = $this->extractItemValues($val, $context);
 
-                            $output[$instance->getName()] = $processedCollectionItems;
+                        if (count($flattenedVal) === 0) {
+                            throw new LogicException(sprintf('Cannot pick a value from "%s" as no properties has been serialized from object.', get_class($val)));
                         }
 
-
-                        if ($instance->isStringable()) {
-                            $output[$instance->getName()] = (string) $val;
+                        if (count($flattenedVal) === 1) {
+                            $output[$instance->getName()] = reset($flattenedVal);
                             continue;
                         }
 
-                        if ($instance->isFlat()) {
-                            if ($val === null) {
-                                $output[$instance->getName()] = null;
-                                continue;
+                        if (count($flattenedVal) > 1) {
+                            if ($instance->getFlatProperty() === null) {
+                                throw new LogicException('$flatProperty cannot be null when there is more than one element to choose.');
                             }
-
-                            $flattenedVal = $this->extractItemValues($val, $context);
-
-                            if (count($flattenedVal) === 0) {
-                                throw new LogicException(sprintf('Cannot pick a value from "%s" as no properties has been serialized from object.', get_class($val)));
-                            }
-
-                            if (count($flattenedVal) === 1) {
-                                $output[$instance->getName()] = reset($flattenedVal);
-                                continue;
-                            }
-
-                            if (count($flattenedVal) > 1) {
-                                if ($instance->getFlatProperty() === null) {
-                                    throw new LogicException('$flatProperty cannot be null when there is more than one element to choose.');
-                                }
-                                $output[$instance->getName()] = $flattenedVal[$instance->getFlatProperty()];
-                            }
+                            $output[$instance->getName()] = $flattenedVal[$instance->getFlatProperty()];
                         }
+                    }
 
-                        if (is_object($val)) {
-                            $output[$instance->getName()] = $this->extractItemValues($val, $context);
-                        }
+                    if (is_object($val)) {
+                        $output[$instance->getName()] = $this->extractItemValues($val, $context);
                     }
                 }
             }
