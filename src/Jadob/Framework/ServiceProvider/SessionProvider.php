b58e4655d6f5f2f752f14cb5c77898c65920f3b0
pizzaminded <pizzaminded@users.noreply.github.com>
2025-03-29 23:45:19 +0100

    refactor(session): add service provider for session

---
+++ src/Jadob/Framework/ServiceProvider/SessionProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Jadob\Framework\ServiceProvider;
+
+use Jadob\Contracts\DependencyInjection\ServiceProviderInterface;
+use Jadob\Core\Session\SessionHandlerFactory;
+use Psr\Container\ContainerInterface;
+use Symfony\Component\HttpFoundation\Session\Storage\NativeSessionStorage;
+use Symfony\Component\HttpFoundation\Session\Storage\SessionStorageInterface;
+
+class SessionProvider implements ServiceProviderInterface
+{
+
+    public function getConfigNode(): ?string
+    {
+        return null;
+    }
+
+    public function register(ContainerInterface $container, object|array|null $config = null): array
+    {
+        return [
+            SessionHandlerFactory::class => function () {
+                return new SessionHandlerFactory();
+            },
+
+            SessionStorageInterface::class => function (ContainerInterface $container) {
+                /** @var SessionHandlerFactory $sessionHandlerFactory */
+                $sessionHandlerFactory = $container->get(SessionHandlerFactory::class);
+                return new NativeSessionStorage(
+                    [],
+                    $sessionHandlerFactory->create()
+                );
+            }
+        ];
+    }
+}
