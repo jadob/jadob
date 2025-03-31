b0779bbcbf302fd72a75b8b10b0819e949a385f2
pizzaminded <pizzaminded@users.noreply.github.com>
2025-04-01 00:43:30 +0200

    refactor(event-dispatcher): use dedicated logger for events

--- src/Jadob/EventDispatcher/EventDispatcher.php
+++ src/Jadob/EventDispatcher/EventDispatcher.php
@@ -28,19 +28,10 @@ class EventDispatcher implements EventDispatcherInterface
      */
     protected array $listeners = [];
 
-    /**
-     * @var LoggerInterface|null
-     */
-    protected ?LoggerInterface $logger;
-
-    /**
-     * EventDispatcher constructor.
-     *
-     * @param LoggerInterface|null $logger
-     */
-    public function __construct(?LoggerInterface $logger = null)
+    public function __construct(
+        private ?LoggerInterface $eventLogger = null
+    )
     {
-        $this->logger = $logger;
     }
 
     /**
@@ -105,11 +96,11 @@ class EventDispatcher implements EventDispatcherInterface
      */
     private function log(string $message, array $context = []): void
     {
-        if ($this->logger === null) {
+        if ($this->eventLogger === null) {
             return;
         }
 
-        $this->logger->info($message, $context);
+        $this->eventLogger->info($message, $context);
     }
 
     /**
