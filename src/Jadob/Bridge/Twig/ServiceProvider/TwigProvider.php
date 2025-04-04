a4b3b9b762ee577bf4bd814ef8acf6decb6190c8
pizzaminded <pizzaminded@users.noreply.github.com>
2025-04-04 22:55:26 +0200

    refactor(bridge-twig): move TwigProvider back to original namespace

---
+++ src/Jadob/Bridge/Twig/ServiceProvider/TwigProvider.php
@@ -0,0 +1,179 @@
+<?php
+declare(strict_types=1);
+
+namespace Jadob\Bridge\Twig\ServiceProvider;
+
+use Closure;
+use Jadob\Bridge\Twig\AppContext;
+use Jadob\Bridge\Twig\Extension\AliasedAssetPathExtension;
+use Jadob\Bridge\Twig\Extension\DebugExtension;
+use Jadob\Bridge\Twig\Extension\PathExtension;
+use Jadob\Bridge\Twig\Extension\TranslationExtension;
+use Jadob\Bridge\Twig\Extension\WebpackManifestAssetExtension;
+use Jadob\Container\Container;
+use Jadob\Container\ParameterStore;
+use Jadob\Contracts\DependencyInjection\ParentServiceProviderInterface;
+use Jadob\Contracts\DependencyInjection\ServiceProviderInterface;
+use Jadob\Core\BootstrapInterface;
+use Jadob\Framework\ServiceProvider\SymfonyTranslatorProvider;
+use Jadob\Router\Router;
+use Psr\Container\ContainerInterface;
+use ReflectionClass;
+use Symfony\Bridge\Twig\Form\TwigRendererEngine;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+use Twig\Loader\LoaderInterface;
+use function file_get_contents;
+use function json_decode;
+
+/**
+ * @author  pizzaminded <mikolajczajkowsky@gmail.com>
+ * @license MIT
+ */
+class TwigProvider implements ServiceProviderInterface, ParentServiceProviderInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigNode(): ?string
+    {
+        return 'twig';
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return (Closure|Closure)[]
+     *
+     * @psalm-return array{Twig\Loader\LoaderInterface: \Closure(ContainerInterface):FilesystemLoader, Twig\Environment: \Closure(ContainerInterface):Environment}
+     * @throws \Twig\Error\LoaderError
+     *
+     */
+    public function register(ContainerInterface $container, array|null|object $config = null): array
+    {
+        $loaderClosure = static function (Container $container) use ($config): \Twig\Loader\FilesystemLoader {
+            $loader = new FilesystemLoader();
+
+            //Adds Jadob namespace with some predefined templates (forms, alerts)
+            $loader->addPath(dirname((new ReflectionClass(AppContext::class))->getFileName()) . '/templates', 'Jadob');
+
+            //@TODO: create some in-framework forms and remove twigbridge
+            //@TODO after refactoring, add some doc about integrating with twig-bridge
+            //Integrates with symfony/twig-bridge for default symfony forms
+            $twigBridgeDirectory = dirname((new ReflectionClass(TwigRendererEngine::class))->getFileName());
+            $formTemplatesDirectory = $twigBridgeDirectory . '/../Resources/views/Form';
+            $loader->addPath($formTemplatesDirectory, FilesystemLoader::MAIN_NAMESPACE);
+
+            foreach ($config['templates_paths'] as $key => $path) {
+                if (\is_int($key)) {
+                    $key = FilesystemLoader::MAIN_NAMESPACE;
+                }
+                $loader->addPath($container->get(BootstrapInterface::class)->getRootDir() . '/' . \ltrim($path, '/'), $key);
+            }
+
+            return $loader;
+        };
+
+        $environmentClosure = static function (
+            LoaderInterface $loader,
+            ParameterStore  $parameterStore,
+            Container       $container,
+        ) use ($config): \Twig\Environment {
+            $cache = false;
+
+            if ($config['cache']) {
+                $cache = sprintf(
+                    '%s/twig',
+                    $parameterStore->get('env_cache_dir')
+                );
+            }
+
+            $options = [
+                'cache' => $cache,
+                'strict_variables' => $config['strict_variables'],
+                'auto_reload' => true
+            ];
+
+            $environment = new Environment(
+                $loader,
+                $options
+            );
+
+            if (isset($config['globals']) && \is_array($config['globals'])) {
+                foreach ($config['globals'] as $globalKey => $globalValue) {
+                    $environment->addGlobal($globalKey, $globalValue);
+                }
+            }
+
+            $extensions = $container->getTaggedServices('twig.extension');
+            foreach ($extensions as $extension) {
+                $environment->addExtension($extension);
+            }
+
+            return $environment;
+        };
+
+        $services = [
+            LoaderInterface::class => $loaderClosure,
+            Environment::class => $environmentClosure,
+            'twig.path_extension' => [
+                'tags' => ['twig.extension'],
+                'factory' => static function (Router $router): PathExtension {
+                    return new PathExtension($router);
+                }
+            ],
+            'twig.debug_extension' => [
+                'tags' => ['twig.extension'],
+                'class' => DebugExtension::class
+            ],
+            'twig.aliased_path_extension' => [
+                'tags' => ['twig.extension'],
+                'factory' => static function () use ($config): AliasedAssetPathExtension {
+                    return new AliasedAssetPathExtension($config['extensions']['aliased_paths'] ?? []);
+                }
+            ],
+        ];
+
+
+        if (isset($config['extenstions']['webpack_manifest'])) {
+            $services['twig.webpack_manifest_extension'] = [
+                'tags' => ['twig.extension'],
+                'factory' => static function (ParameterStore $parameterStore) use ($config): WebpackManifestAssetExtension {
+                    $webpackManifestConfig = $config['extensions']['webpack_manifest'];
+                    $manifestJsonLocation =
+                        sprintf('%s/%s',
+                            $parameterStore->get('root_dir'),
+                            ltrim((string)$webpackManifestConfig['manifest_json_location'], '/')
+                        );
+
+                    $manifest = json_decode(
+                        file_get_contents($manifestJsonLocation),
+                        true,
+                        512,
+                        JSON_THROW_ON_ERROR
+                    );
+
+                    return new WebpackManifestAssetExtension($manifest);
+                }
+            ];
+        }
+
+
+        $services['twig.translator_extension'] = [
+            'tags' => ['twig.extension'],
+            'factory' => static function (TranslatorInterface $translator): TranslationExtension {
+                return new TranslationExtension($translator);
+            }
+        ];
+
+        return $services;
+    }
+
+    public function getParentServiceProviders(): array
+    {
+        return [
+            SymfonyTranslatorProvider::class
+        ];
+    }
+}
