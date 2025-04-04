16c43c3144f82a2b4d0da9b7e945b8514f8627dd
pizzaminded <pizzaminded@users.noreply.github.com>
2025-04-04 23:29:14 +0200

    refactor(bridge-symfony-validator): introduce `TwigExtensionProvider`

---
+++ src/Jadob/Bridge/Twig/Container/ExtensionProvider/TwigExtensionProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Jadob\Bridge\Twig\Container\ExtensionProvider;
+
+use Jadob\Bridge\Twig\Container\Extension\TwigExtension;
+use Jadob\Contracts\DependencyInjection\ContainerExtensionProviderInterface;
+use Psr\Container\ContainerInterface;
+
+class TwigExtensionProvider implements ContainerExtensionProviderInterface
+{
+
+    public function getAutowiringExtensions(ContainerInterface $container): array
+    {
+        return [];
+    }
+
+    public function getContainerExtensions(): array
+    {
+       return [
+           new TwigExtension()
+       ];
+    }
+
+}
