d89a7def099b8e209af56599a91a8ee9829194a4
pizzaminded <pizzaminded@users.noreply.github.com>
2025-04-04 23:28:46 +0200

    refactor(bridge-twig): introduce `TwigModule`

---
+++ src/Jadob/Bridge/Twig/Module/TwigModule.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Jadob\Bridge\Twig\Module;
+
+use Jadob\Bridge\Twig\Container\ExtensionProvider\TwigExtensionProvider;
+use Jadob\Bridge\Twig\ServiceProvider\TwigProvider;
+use Jadob\Contracts\Framework\Module\ModuleInterface;
+use Psr\Container\ContainerInterface;
+
+class TwigModule implements ModuleInterface
+{
+
+    public function getServiceProviders(string $env): array
+    {
+        return [
+            new TwigProvider()
+        ];
+    }
+
+    public function getContainerExtensionProviders(string $env): array
+    {
+       return [
+           new TwigExtensionProvider()
+       ];
+    }
+
+    public function getEventListeners(ContainerInterface $container, string $env): array
+    {
+        // TODO: Implement getEventListeners() method.
+    }
+}
