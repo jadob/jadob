729580a2fefce2395c2a86aa1b136a88d180eff9
pizzaminded <pizzaminded@users.noreply.github.com>
2025-04-04 23:34:26 +0200

    feat(bridge-symfony-validator): add first implementation

--- src/Jadob/Bridge/Symfony/Validator/ServiceProvider/SymfonyValidatorProvider.php
+++ src/Jadob/Bridge/Symfony/Validator/ServiceProvider/SymfonyValidatorProvider.php
@@ -4,6 +4,8 @@ namespace Jadob\Bridge\Symfony\Validator\ServiceProvider;
 
 use Jadob\Contracts\DependencyInjection\ServiceProviderInterface;
 use Psr\Container\ContainerInterface;
+use Symfony\Component\Validator\Validation;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class SymfonyValidatorProvider implements ServiceProviderInterface
 {
@@ -15,6 +17,10 @@ class SymfonyValidatorProvider implements ServiceProviderInterface
 
     public function register(ContainerInterface $container, object|array|null $config = null): array
     {
-
+        return [
+            ValidatorInterface::class => static function() {
+                return Validation::createValidatorBuilder()->getValidator();
+            }
+        ];
     }
 }
