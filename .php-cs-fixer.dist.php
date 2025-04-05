baa2987f3eb4526ef837c90d40d18850365e75a2
pizzaminded <pizzaminded@users.noreply.github.com>
2025-04-05 22:06:43 +0200

    fix: remove redundant rule

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -28,7 +28,6 @@ return $config
         'fully_qualified_strict_types' => true,
         'global_namespace_import' => true,
         'no_unused_imports' => true,
-        'ordered_imports' => true,
         'single_import_per_statement' => true,
         'single_line_after_imports' => true,
         'list_syntax' => true,
