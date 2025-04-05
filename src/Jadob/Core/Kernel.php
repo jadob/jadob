e348513fff2926d964e2e8f3469b2a826eb9b8a3
pizzaminded <pizzaminded@users.noreply.github.com>
2025-04-05 21:57:35 +0200

    v0.7.3

--- src/Jadob/Core/Kernel.php
+++ src/Jadob/Core/Kernel.php
@@ -51,7 +51,7 @@ class Kernel
      * @see https://semver.org/
      * @var string
      */
-    public const VERSION = '0.7.2';
+    public const VERSION = '0.7.3';
 
     protected RuntimeInterface $runtime;
 
