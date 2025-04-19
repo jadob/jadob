6d7f43a3b6b8f98560622859a088b41795ce6e30
pizzaminded <pizzaminded@users.noreply.github.com>
2025-04-19 21:53:53 +0200

    refactor: expose helper set as a another service

--- src/Jadob/Framework/ServiceProvider/ConsoleProvider.php
+++ src/Jadob/Framework/ServiceProvider/ConsoleProvider.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace Jadob\Framework\ServiceProvider;
 
-use Jadob\Container\Container;
 use Jadob\Contracts\DependencyInjection\ServiceProviderInterface;
 use Jadob\Core\Kernel;
 use Psr\Container\ContainerInterface;
 use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Helper\HelperSet;
 
 /**
  * Adds symfony/console functionality to framework.
@@ -40,7 +40,21 @@ class ConsoleProvider implements ServiceProviderInterface
          * We should not rely on PHP_SAPI here
          */
         if (strtolower(PHP_SAPI) === 'cli') {
-            return ['console' => new Application('Jadob', Kernel::VERSION)];
+            return [
+                Application::class => [
+                    'factory' => function (HelperSet $helperSet) {
+                        $application = new Application('Jadob', Kernel::VERSION);
+                        $application->setHelperSet($helperSet);
+
+                        return $application;
+                    }
+                ],
+                HelperSet::class => [
+                    'factory' => function () {
+                        return new HelperSet();
+                    }
+                ]
+            ];
         }
 
         return [];
