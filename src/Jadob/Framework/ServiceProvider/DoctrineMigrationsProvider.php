2cbe79141319781932fad245e779b3e194200012
pizzaminded <pizzaminded@users.noreply.github.com>
2025-04-19 21:54:17 +0200

    refactor(bridge-doctrine-migrations): rework console commands

--- src/Jadob/Framework/ServiceProvider/DoctrineMigrationsProvider.php
+++ src/Jadob/Framework/ServiceProvider/DoctrineMigrationsProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Jadob\Framework\ServiceProvider;
 
 use Doctrine\Migrations\Configuration\EntityManager\ExistingEntityManager;
+use Doctrine\Migrations\Configuration\EntityManager\ManagerRegistryEntityManager;
 use Doctrine\Migrations\Configuration\Migration\ConfigurationArray;
 use Doctrine\Migrations\DependencyFactory;
 use Doctrine\Migrations\Tools\Console\Command\DiffCommand;
@@ -17,9 +18,11 @@ use Doctrine\Migrations\Tools\Console\Command\RollupCommand;
 use Doctrine\Migrations\Tools\Console\Command\StatusCommand;
 use Doctrine\Migrations\Tools\Console\Command\SyncMetadataCommand;
 use Doctrine\Migrations\Tools\Console\Command\VersionCommand;
+use Doctrine\Persistence\ManagerRegistry;
 use Jadob\Container\Container;
 use Jadob\Contracts\DependencyInjection\ParentServiceProviderInterface;
 use Jadob\Contracts\DependencyInjection\ServiceProviderInterface;
+use Jadob\Framework\Logger\LoggerFactory;
 use LogicException;
 use Psr\Container\ContainerInterface;
 use Symfony\Component\Console\Application;
@@ -34,54 +37,96 @@ class DoctrineMigrationsProvider implements ServiceProviderInterface, ParentServ
     public function register(ContainerInterface $container, null|object|array $config = null): array
     {
         $output = [];
-        $ormMigrations = $config['orm'];
-        foreach ($ormMigrations as $managerName => $migrationConfig) {
-            $factory = static function (ContainerInterface $container) use ($managerName, $migrationConfig): DependencyFactory {
-                $migrationConfigObj = new ConfigurationArray($migrationConfig);
-                $em = $container->get(sprintf('doctrine.orm.%s', $managerName));
+        $output[DependencyFactory::class] = static function (
+            ContainerInterface $container,
+            ManagerRegistry    $managerRegistry,
+            LoggerFactory      $loggerFactory,
+        ) use (
+            $config
+        ): DependencyFactory {
+            $migrationConfigObj = new ConfigurationArray($config);
 
-                return DependencyFactory::fromEntityManager(
-                    $migrationConfigObj,
-                    new ExistingEntityManager($em)
-                );
-            };
+            return DependencyFactory::fromEntityManager(
+                $migrationConfigObj,
+                ManagerRegistryEntityManager::withSimpleDefault(
+                    $managerRegistry,
+                    $managerRegistry->getDefaultManagerName()
+                ),
+                $loggerFactory->getDefaultLogger()
+            );
+        };
 
+        $output[DumpSchemaCommand::class] = [
+            'tags' => ['console.command'],
+            'factory' => function (DependencyFactory $dependencyFactory) {
+                return new DumpSchemaCommand($dependencyFactory);
+            }
+        ];
 
-            $output[sprintf('doctrine.migrations.%s', $managerName)] = $factory;
-        }
+        $output[DiffCommand::class] = [
+            'tags' => ['console.command'],
+            'factory' => function (DependencyFactory $dependencyFactory) {
+                return new DiffCommand($dependencyFactory);
+            }
+        ];
+
+        $output[ExecuteCommand::class] = [
+            'tags' => ['console.command'],
+            'factory' => function (DependencyFactory $dependencyFactory) {
+                return new ExecuteCommand($dependencyFactory);
+            }
+        ];
+
+        $output[LatestCommand::class] = [
+            'tags' => ['console.command'],
+            'factory' => function (DependencyFactory $dependencyFactory) {
+                return new LatestCommand($dependencyFactory);
+            }
+        ];
+
+        $output[ListCommand::class] = [
+            'tags' => ['console.command'],
+            'factory' => function (DependencyFactory $dependencyFactory) {
+                return new ListCommand($dependencyFactory);
+            }
+        ];
+
+        $output[MigrateCommand::class] = [
+            'tags' => ['console.command'],
+            'factory' => function (DependencyFactory $dependencyFactory) {
+                return new MigrateCommand($dependencyFactory);
+            }
+        ];
+
+        $output[RollupCommand::class] = [
+            'tags' => ['console.command'],
+            'factory' => function (DependencyFactory $dependencyFactory) {
+                return new RollupCommand($dependencyFactory);
+            }
+        ];
+
+        $output[StatusCommand::class] = [
+            'tags' => ['console.command'],
+            'factory' => function (DependencyFactory $dependencyFactory) {
+                return new StatusCommand($dependencyFactory);
+            }
+        ];
+
+        $output[SyncMetadataCommand::class] = [
+            'tags' => ['console.command'],
+            'factory' => function (DependencyFactory $dependencyFactory) {
+                return new SyncMetadataCommand($dependencyFactory);
+            }
+        ]; $output[VersionCommand::class] = [
+            'tags' => ['console.command'],
+            'factory' => function (DependencyFactory $dependencyFactory) {
+                return new VersionCommand($dependencyFactory);
+            }
+        ];
 
         return $output;
     }
 
-    public function onContainerBuild(Container $container, $config)
-    {
-        if ($container->has('console')) {
-            /** @var Application $console */
-            $console = $container->get('console');
-
-            if (!isset($config['orm'])) {
-                throw new LogicException('You need to configure ORM to use migrations.');
-            }
-
-            $ormManagers = array_keys($config['orm']);
-            foreach ($ormManagers as $managerName) {
-                /** @var DependencyFactory $depFactory */
-                $depFactory = $container->get(sprintf('doctrine.migrations.%s', $managerName));
-
-                $console->add(new DumpSchemaCommand($depFactory, sprintf('migrations:%s:dump-schema', $managerName)));
-                $console->add(new DiffCommand($depFactory, sprintf('migrations:%s:diff', $managerName)));
-                $console->add(new ExecuteCommand($depFactory, sprintf('migrations:%s:execute', $managerName)));
-                $console->add(new GenerateCommand($depFactory, sprintf('migrations:%s:generate', $managerName)));
-                $console->add(new LatestCommand($depFactory, sprintf('migrations:%s:latest', $managerName)));
-                $console->add(new ListCommand($depFactory, sprintf('migrations:%s:list', $managerName)));
-                $console->add(new MigrateCommand($depFactory, sprintf('migrations:%s:migrate', $managerName)));
-                $console->add(new RollupCommand($depFactory, sprintf('migrations:%s:rollup', $managerName)));
-                $console->add(new StatusCommand($depFactory, sprintf('migrations:%s:status', $managerName)));
-                $console->add(new SyncMetadataCommand($depFactory, sprintf('migrations:%s:sync-metadata-storage', $managerName)));
-                $console->add(new VersionCommand($depFactory, sprintf('migrations:%s:version', $managerName)));
-            }
-        }
-    }
 
     public function getParentServiceProviders(): array
     {
