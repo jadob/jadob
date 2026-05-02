adcf427f7884696acce4dbba6043254b52fa9a51
pizzaminded <pizzaminded@users.noreply.github.com>
2026-05-02 19:44:11 +0200

    feat(firewall): prevent stateless firewalls from having identity stacking enabled

--- src/Jadob/Auth/Firewall/Firewall.php
+++ src/Jadob/Auth/Firewall/Firewall.php
@@ -1,7 +1,9 @@
 <?php
+declare(strict_types=1);
 
 namespace Jadob\Auth\Firewall;
 
+use Jadob\Auth\AuthenticatorInterface;
 use Jadob\Auth\EntryPointInterface;
 use Jadob\Auth\Identity\IdentityProviderInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -9,15 +11,33 @@ use Symfony\Component\HttpFoundation\RequestMatcherInterface;
 
 class Firewall
 {
+    /**
+     * @param string $name
+     * @param RequestMatcherInterface $requestMatcher
+     * @param array<AuthenticatorInterface> $authenticators
+     * @param IdentityProviderInterface $identityProvider
+     * @param EntryPointInterface|null $entryPoint
+     * @param bool $stateless
+     * @throws FirewallConfigurationException
+     */
     public function __construct(
         private(set) string                    $name,
         private(set) RequestMatcherInterface   $requestMatcher,
         private(set) array                     $authenticators,
         private(set) IdentityProviderInterface $identityProvider,
         private(set) ?EntryPointInterface      $entryPoint = null,
-        private(set) bool                      $stateless = false
+        private(set) bool                      $stateless = false,
+        private(set) bool                      $identityStackingEnabled = false
     )
     {
+        if ($this->stateless && $this->identityStackingEnabled) {
+            throw new FirewallConfigurationException(
+                sprintf(
+                    'Identity stacking is not available for stateless firewalls. Set identityStackingEnabled to false for
+                    firewall %s or make it stateful.', $this->name,
+                )
+            );
+        }
     }
 
     /**
---
+++ src/Jadob/Auth/Firewall/FirewallConfigurationException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Jadob\Auth\Firewall;
+
+use Jadob\Auth\Exception\AuthenticationException;
+
+class FirewallConfigurationException extends AuthenticationException
+{
+
+}
