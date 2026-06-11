4d92e4b0fbbfe76bcff167f3526f446127d5d707
pizzaminded <pizzaminded@users.noreply.github.com>
2026-06-11 23:20:53 +0200

    auth: add phpdocs to methods

--- src/Jadob/Auth/EntryPointInterface.php
+++ src/Jadob/Auth/EntryPointInterface.php
@@ -8,5 +8,11 @@ use Symfony\Component\HttpFoundation\Response;
 
 interface EntryPointInterface
 {
+    /**
+     * Called when no authenticators in firewall are supporting the request.
+     * You can redirect user to login or return unauthenticated api user here.
+     * @param Request $request
+     * @return Response
+     */
     public function commence(Request $request): Response;
 }
