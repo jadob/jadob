49aaf15038a48e82d14e61819fe068c59c398f7c
pizzaminded <pizzaminded@users.noreply.github.com>
2026-06-11 22:28:46 +0200

    auth: add `FirewallConfig` implementation

--- src/Jadob/Auth/ServiceProvider/FirewallConfig.php
+++ src/Jadob/Auth/ServiceProvider/FirewallConfig.php
@@ -6,12 +6,76 @@ namespace Jadob\Auth\ServiceProvider;
 class FirewallConfig
 {
     private array $authenticators = [];
-    private ?string $successHandlerServiceId = null;
-    private ?string $failureHandlerServiceId = null;
-    private ?bool $stateless = null;
+    private bool $stateless = false;
+    private bool $identityStackingEnabled = false;
+    private(set) ?string $entryPointServiceId = null;
+    private(set) ?string $requestMatcherServiceId = null;
+    private(set) ?string $identityProviderServiceId = null;
+    private(set) ?string $identityPickerServiceId = null;
 
     public function __construct(
         private string $name,
-    ) {
+    )
+    {
+    }
+
+    public function withEntryPointServiceId(string $entryPointServiceId): self
+    {
+        $this->entryPointServiceId = $entryPointServiceId;
+        return $this;
+    }
+
+    public function withRequestMatcherServiceId(string $requestMatcherServiceId): self
+    {
+        $this->requestMatcherServiceId = $requestMatcherServiceId;
+        return $this;
+    }
+
+    public function withAuthenticator(string $authenticatorServiceId): self
+    {
+        $this->authenticators[] = $authenticatorServiceId;
+        return $this;
+    }
+
+    public function enableStateless(): self
+    {
+        $this->stateless = true;
+        return $this;
+    }
+
+    public function enableIdentityStacking(): self
+    {
+        $this->identityStackingEnabled = true;
+        return $this;
+    }
+
+    /**
+     * @return array<string|class-string>
+     */
+    public function getAuthenticators(): array
+    {
+        return $this->authenticators;
+    }
+
+    public function isStateless(): bool
+    {
+        return $this->stateless;
+    }
+
+    public function isIdentityStackingEnabled(): bool
+    {
+        return $this->identityStackingEnabled;
+    }
+
+    public function withIdentityProviderServiceId(string $identityProviderServiceId): self
+    {
+        $this->identityProviderServiceId = $identityProviderServiceId;
+        return $this;
+    }
+
+    public function withIdentityPickerServiceId(string $identityPickerServiceId): self
+    {
+        $this->identityPickerServiceId = $identityPickerServiceId;
+        return $this;
     }
 }
