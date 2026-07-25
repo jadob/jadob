eba440f8ca663c97015ed096fa73bdcca7828594
pizzaminded <pizzaminded@users.noreply.github.com>
2026-07-25 22:38:15 +0200

    container: move `getConfigNode` to `ConfigObjectProviderInterface`
    `getConfigNode` in `ServiceProviderInterface` was returning null in 95% of use cases. Placing it in `ConfigObjectProviderInterface` has more sense as this interface is always included with service provider when there is a config required.

--- src/Jadob/Contracts/DependencyInjection/ConfigObjectProviderInterface.php
+++ src/Jadob/Contracts/DependencyInjection/ConfigObjectProviderInterface.php
@@ -5,5 +5,11 @@ namespace Jadob\Contracts\DependencyInjection;
 
 interface ConfigObjectProviderInterface
 {
-    public function getDefaultConfigurationObject(): object;
+    /**
+     * If your services require some configuration, you can create a config node, return its name here, and it will
+     * be passed further into register() method.
+     */
+    public function getConfigNode(): string;
+
+    public function getDefaultConfigurationObject(): ConfigNode;
 }
--- src/Jadob/Contracts/DependencyInjection/ServiceProviderInterface.php
+++ src/Jadob/Contracts/DependencyInjection/ServiceProviderInterface.php
@@ -9,14 +9,6 @@ use Psr\Container\ContainerInterface;
 interface ServiceProviderInterface
 {
     /**
-     * If your services require some configuration, you can create a config node, return its name here and it will
-     * be passed further into register() method.
-     * @return string|null
-     */
-    public function getConfigNode(): ?string;
-
-
-    /**
      * Configure and register your services here.
      *
      * @param ContainerInterface $container
