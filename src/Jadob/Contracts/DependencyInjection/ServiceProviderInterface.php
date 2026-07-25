543466be57aff3318d943ee0db35ce850ffe9894
pizzaminded <pizzaminded@users.noreply.github.com>
2026-07-26 01:19:32 +0200

    container: change service provider contract

--- src/Jadob/Contracts/DependencyInjection/ServiceProviderInterface.php
+++ src/Jadob/Contracts/DependencyInjection/ServiceProviderInterface.php
@@ -4,16 +4,22 @@ declare(strict_types=1);
 
 namespace Jadob\Contracts\DependencyInjection;
 
+use Jadob\Container\Config\ConfigNodeInterface;
+
+/**
+ * Groups related build-time service registrations behind a reusable,
+ * stateless package extension point.
+ */
 interface ServiceProviderInterface
 {
     /**
      * Configure and register your services here.
      *
-     * @param ContainerInterface $container
-     * @param ConfigNode|null $config
+     * @param ContainerBuilderInterface $builder
+     * @param ConfigNodeInterface|null $config
      */
     public function register(
         ContainerBuilderInterface $builder,
-        ConfigNode|null $config = null,
+        ConfigNodeInterface|null  $config = null,
     ): void;
 }
