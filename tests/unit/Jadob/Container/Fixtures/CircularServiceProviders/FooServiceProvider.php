8a2e0f4f7bebebfffd90770568a0d988325c9009
pizzaminded <pizzaminded@users.noreply.github.com>
2026-07-25 22:47:12 +0200

    container: apply new contract structure for test fixtures

--- tests/unit/Jadob/Container/Fixtures/CircularServiceProviders/FooServiceProvider.php
+++ tests/unit/Jadob/Container/Fixtures/CircularServiceProviders/FooServiceProvider.php
@@ -3,28 +3,25 @@ declare(strict_types=1);
 
 namespace Jadob\Container\Fixtures\CircularServiceProviders;
 
+use Jadob\Contracts\DependencyInjection\ConfigNode;
+use Jadob\Contracts\DependencyInjection\ContainerBuilderInterface;
 use Jadob\Contracts\DependencyInjection\ParentServiceProviderInterface;
 use Jadob\Contracts\DependencyInjection\ServiceProviderInterface;
-use Psr\Container\ContainerInterface;
 
-class FooServiceProvider implements ServiceProviderInterface, ParentServiceProviderInterface
+final readonly class FooServiceProvider implements ServiceProviderInterface, ParentServiceProviderInterface
 {
     public function getParentServiceProviders(): array
     {
-        return [BarServiceProvider::class];
+        return [
+            BarServiceProvider::class
+        ];
     }
 
-    public function getConfigNode(): ?string
+    public function register(
+        ContainerBuilderInterface $builder,
+        ?ConfigNode $config = null
+    ): void
     {
-        return 'foo';
-    }
 
-    /**
-     * @param array<mixed> $config
-     * @return array<string, \Closure>
-     */
-    public function register(ContainerInterface $container, object|array|null $config = null): array
-    {
-        return [];
     }
 }
