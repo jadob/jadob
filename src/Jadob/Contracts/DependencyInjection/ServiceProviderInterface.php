379f1f2512984e17f9b411150f6c6ac597586e88
pizzaminded <pizzaminded@users.noreply.github.com>
2026-07-25 22:39:52 +0200

    container: use `ContainerBuilderInterface` and `ConfigNode` in `register` method

--- src/Jadob/Contracts/DependencyInjection/ServiceProviderInterface.php
+++ src/Jadob/Contracts/DependencyInjection/ServiceProviderInterface.php
@@ -1,10 +1,8 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Jadob\Contracts\DependencyInjection;
-
-use Closure;
-use Psr\Container\ContainerInterface;
 
 interface ServiceProviderInterface
 {
@@ -12,11 +10,10 @@ interface ServiceProviderInterface
      * Configure and register your services here.
      *
      * @param ContainerInterface $container
-     * @param array|object|null $config
-     * @return array<string|class-string, array|Closure|object>
+     * @param ConfigNode|null $config
      */
     public function register(
-        ContainerInterface $container,
-        array|object|null $config = null
-    ): array;
+        ContainerBuilderInterface $builder,
+        ConfigNode|null $config = null,
+    ): void;
 }
