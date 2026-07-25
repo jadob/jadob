de84e149bc64063af509aa200165623ed5f94cae
pizzaminded <pizzaminded@users.noreply.github.com>
2026-07-26 01:19:50 +0200

    container: add `ConfigNodeFinderInterface`

---
+++ src/Jadob/Container/Config/ConfigNodeFinderInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Jadob\Container\Config;
+
+interface ConfigNodeFinderInterface
+{
+    /**
+     * Returns an array because there may be multiple nodes (e.g. base config, production, enterprise customer config).
+     * Nodes should be returned in ascending-priority order.
+     * @param string $node
+     * @return array<ConfigNodeInterface>
+     */
+    public function find(string $node): array;
+}
