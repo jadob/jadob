9f315fde6b65450cd0d97e7038ef92b7112e1ac0
pizzaminded <pizzaminded@users.noreply.github.com>
2026-08-01 22:57:15 +0200

    bridge-symfony-translator: modernize `TranslationSource` class

--- src/Jadob/Bridge/Symfony/Translation/TranslationSource.php
+++ src/Jadob/Bridge/Symfony/Translation/TranslationSource.php
@@ -4,49 +4,17 @@ declare(strict_types=1);
 namespace Jadob\Bridge\Symfony\Translation;
 
 /**
- * Do not rely on this class as it is internal.
- * Internal things are not supposed to use outside of the framework codebase and may be removed in any time.
- *
  * @internal
  * @author pizzaminded <mikolajczajkowsky@gmail.com>
  * @license MIT
  */
-class TranslationSource
+final readonly class TranslationSource
 {
-    /**
-     * TranslationSource constructor.
-     * @param $path
-     * @param $locale
-     * @param $domain
-     * @param string $path
-     * @param string $locale
-     * @param string $domain
-     */
-    public function __construct(private readonly string $path, private readonly string $locale, private readonly string $domain)
+    public function __construct(
+        private(set) string $path,
+        private(set) string $locale,
+        private(set) string $domain
+    )
     {
-    }
-
-    /**
-     * @return string
-     */
-    public function getPath(): string
-    {
-        return $this->path;
-    }
-
-    /**
-     * @return string
-     */
-    public function getLocale(): string
-    {
-        return $this->locale;
-    }
-
-    /**
-     * @return string
-     */
-    public function getDomain(): string
-    {
-        return $this->domain;
     }
 }
