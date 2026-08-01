97c8128161ad603b5f42bd20ce8d763732bb424b
pizzaminded <pizzaminded@users.noreply.github.com>
2026-08-01 22:25:44 +0200

    bridge-symfony-validator: refactor to new container contract

--- src/Jadob/Bridge/Symfony/Validator/ServiceProvider/SymfonyValidatorProvider.php
+++ src/Jadob/Bridge/Symfony/Validator/ServiceProvider/SymfonyValidatorProvider.php
@@ -1,26 +1,27 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Jadob\Bridge\Symfony\Validator\ServiceProvider;
 
+use Jadob\Container\Config\ConfigNodeInterface;
+use Jadob\Contracts\DependencyInjection\ContainerBuilderInterface;
 use Jadob\Contracts\DependencyInjection\ServiceProviderInterface;
 use Psr\Container\ContainerInterface;
 use Symfony\Component\Validator\Validation;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
-class SymfonyValidatorProvider implements ServiceProviderInterface
+final readonly class SymfonyValidatorProvider implements ServiceProviderInterface
 {
-    public function getConfigNode(): ?string
+    public function register(
+        ContainerBuilderInterface $builder,
+        ?ConfigNodeInterface $config = null
+    ): void
     {
-        return null;
-    }
-
-    public function register(ContainerInterface $container, object|array|null $config = null): array
-    {
-        return [
-            ValidatorInterface::class => static function() {
+        $builder
+            ->set(ValidatorInterface::class)
+            ->factory(function () {
                 return Validation::createValidatorBuilder()->getValidator();
-            }
-        ];
+            });
     }
 }
