829f65563d2667c0b5a7e62460f34da4fe96bd82
pizzaminded <pizzaminded@users.noreply.github.com>
2026-08-10 00:56:06 +0200

    container: add first `ServiceGraphContainer` implementation

--- src/Jadob/Container/ServiceGraphContainer.php
+++ src/Jadob/Container/ServiceGraphContainer.php
@@ -4,10 +4,21 @@ declare(strict_types=1);
 
 namespace Jadob\Container;
 
+use Jadob\Contracts\DependencyInjection\Reference;
+use Jadob\Contracts\DependencyInjection\ReferenceType;
 use Psr\Container\ContainerInterface;
+use function array_map;
+use function call_user_func_array;
+use function is_string;
 
-class ServiceGraphContainer implements  ContainerInterface
+class ServiceGraphContainer implements ContainerInterface
 {
+
+    /**
+     * @var array<string, object>
+     */
+    private array $initialized = [];
+
     public function __construct(
         private ServiceGraph $graph
     )
@@ -16,9 +27,67 @@ class ServiceGraphContainer implements  ContainerInterface
 
     public function get(string $id): object
     {
+        if(isset($this->initialized[$id])) {
+            return $this->initialized[$id];
+        }
+
+        $definition = $this->graph->get($id);
+
+        $args = $this->resolveArgs($definition->arguments);
+
+
+        if($definition->factory === null) {
+            $reflectionClass = new \ReflectionClass($definition->className);
+
+            return $this
+                ->doInitialize(
+                    $id,
+                    $reflectionClass->newInstanceArgs($args),
+                );
+        }
+
+        return $this
+            ->doInitialize(
+                $id,
+                call_user_func_array($definition->factory, $args)
+            );
+    }
+
+    private function doInitialize(
+        string $id,
+        object $service,
+    ): object
+    {
+        $this->initialized[$id] = $service;
+        return $service;
+    }
+
+    /**
+     * @param array<string, Reference|string> $args
+     * @return array
+     */
+    private function resolveArgs(array $args): array
+    {
+        return array_map(
+            function (string|Reference $arg): string|object {
+                if(is_string($arg)) {
+                    return $arg;
+                }
+
+                if($arg->type === ReferenceType::Service) {
+                    return $this->get(
+                        $arg->value
+                    );
+                }
+
+                throw new \LogicException('not implemented');
+            },
+            $args
+        );
     }
 
     public function has(string $id): bool
     {
+        return $this->graph->has($id);
     }
 }
