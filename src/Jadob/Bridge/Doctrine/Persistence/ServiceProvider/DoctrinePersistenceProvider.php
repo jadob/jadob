aa0129dcb9ef0f3d1e7003613cb2b9815732c912
pizzaminded <pizzaminded@users.noreply.github.com>
2026-08-09 23:25:50 +0200

    bridge-doctrine-persistence: apply new service provider contract

--- src/Jadob/Bridge/Doctrine/Persistence/ServiceProvider/DoctrinePersistenceProvider.php
+++ src/Jadob/Bridge/Doctrine/Persistence/ServiceProvider/DoctrinePersistenceProvider.php
@@ -5,28 +5,25 @@ namespace Jadob\Bridge\Doctrine\Persistence\ServiceProvider;
 
 use Doctrine\Persistence\ManagerRegistry;
 use Jadob\Bridge\Doctrine\Persistence\DoctrineManagerRegistry;
-use Jadob\Container\Container;
+use Jadob\Container\Config\ConfigNodeInterface;
+use Jadob\Contracts\DependencyInjection\ContainerBuilderInterface;
 use Jadob\Contracts\DependencyInjection\ServiceProviderInterface;
-use Psr\Container\ContainerInterface;
 
-class DoctrinePersistenceProvider implements ServiceProviderInterface
+final readonly class DoctrinePersistenceProvider implements ServiceProviderInterface
 {
-    public function getConfigNode(): ?string
+    public function register(
+        ContainerBuilderInterface $builder,
+        ?ConfigNodeInterface $config = null
+    ): void
     {
-        return null;
-    }
+        $builder
+            ->set(DoctrineManagerRegistry::class);
 
-    public function register(ContainerInterface $container, array|object|null $config = null): array
-    {
-        return [
-            ManagerRegistry::class => static function (): ManagerRegistry {
-                return new DoctrineManagerRegistry();
-            }
-        ];
-    }
+        $builder
+            ->bind(
+                ManagerRegistry::class,
+                DoctrineManagerRegistry::class
+            );
 
-    public function onContainerBuild(Container $container, $config)
-    {
-        // TODO: Implement onContainerBuild() method.
     }
 }
