8f722ca076f6d183ecd788130a0daa90ab513b1a
pizzaminded <pizzaminded@users.noreply.github.com>
2026-08-16 00:12:05 +0200

    bridge-dynamite: add table configuration

---
+++ src/Jadob/Bridge/Dynamite/ServiceProvider/DynamiteConfig.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Jadob\Bridge\Dynamite\ServiceProvider;
+
+use Jadob\Container\Config\ConfigNodeInterface;
+
+final class DynamiteConfig implements ConfigNodeInterface
+{
+    /**
+     * @param bool $mappingCacheEnabled
+     * @param array<TableConfig> $tableConfigs
+     */
+    public function __construct(
+        private(set) bool $mappingCacheEnabled = false,
+        private(set) array $tableConfigs = []
+    )
+    {
+    }
+
+    public function enableMappingCache(): self
+    {
+        $this->mappingCacheEnabled = true;
+
+        return $this;
+    }
+
+    public function configureTable(): TableConfig
+    {
+        $tableConfig = new TableConfig();
+        $this->tableConfigs[] = $tableConfig;
+
+        return $tableConfig;
+    }
+
+}
---
+++ src/Jadob/Bridge/Dynamite/ServiceProvider/TableConfig.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Jadob\Bridge\Dynamite\ServiceProvider;
+
+final class TableConfig
+{
+    /**
+     * @param string $name
+     * @param string $partitionKeyName
+     * @param string $sortKeyName
+     * @param array<class-string> $managedObjects
+     */
+    public function __construct(
+        private(set) ?string $name = null,
+        private(set) ?string $partitionKeyName = null,
+        private(set) ?string $sortKeyName = null,
+        private(set) array  $managedObjects = []
+    )
+    {
+    }
+
+    public function withName(
+        string $name,
+    ): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function withPartitionKeyName(
+        string $partitionKeyName,
+    ): self
+    {
+        $this->partitionKeyName = $partitionKeyName;
+        return $this;
+    }
+
+    public function withSortKeyName(
+        string $sortKeyName,
+    ): self
+    {
+        $this->sortKeyName = $sortKeyName;
+        return $this;
+    }
+
+    public function withManagedObject(
+        string $managedObject,
+    ): self
+    {
+        $this->managedObjects[] = $managedObject;
+        return $this;
+    }
+}
