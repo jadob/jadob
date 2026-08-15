2f2d681b97c859130d9b24e0909c9fb2c3d86f2c
pizzaminded <pizzaminded@users.noreply.github.com>
2026-08-16 00:45:27 +0200

    framework: add `RotatingFileHandlerFactory`

---
+++ src/Jadob/Framework/Logger/HandlerFactory/RotatingFileHandlerFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Jadob\Framework\Logger\HandlerFactory;
+
+use Monolog\Handler\HandlerInterface;
+use Monolog\Handler\RotatingFileHandler;
+use Monolog\Handler\StreamHandler;
+
+final readonly class RotatingFileHandlerFactory implements LogHandlerFactoryInterface
+{
+    public function create(
+        array $parameters,
+        string $level
+    ): HandlerInterface
+    {
+        return new RotatingFileHandler(
+            filename: $parameters['file'],
+            level: $level,
+        );
+    }
+}
