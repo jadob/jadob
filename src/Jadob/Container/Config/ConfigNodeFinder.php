0e35731e1dd2da1f44bf610d607925023797c8eb
pizzaminded <pizzaminded@users.noreply.github.com>
2026-08-15 23:30:04 +0200

    container: add implementation for config lookup

--- src/Jadob/Container/Config/ConfigNodeFinder.php
+++ src/Jadob/Container/Config/ConfigNodeFinder.php
@@ -2,6 +2,11 @@
 
 namespace Jadob\Container\Config;
 
+use LogicException;
+use function file_exists;
+use function rtrim;
+use function sprintf;
+
 class ConfigNodeFinder implements ConfigNodeFinderInterface
 {
     /**
@@ -13,10 +18,34 @@ class ConfigNodeFinder implements ConfigNodeFinderInterface
     {
     }
 
-    public function find(string $node): array {
-        return [];
-    }
+    public function find(string $node): array
+    {
+        $result = [];
+        foreach ($this->paths as $path) {
+            $path = sprintf('%s/%s.php',
+                rtrim($path, '/'),
+                $node
+            );
 
+            if (file_exists($path) === false) {
+                continue;
+            }
+
+            $config = include $path;
+            if (is_object($config) === false) {
+                throw new LogicException(
+                    sprintf(
+                        'Config file in "%s" must return an object.',
+                        $path
+                    )
+                );
+            }
+
+            $result[] = $config;
+        }
+
+        return $result;
+    }
 
 
 }
