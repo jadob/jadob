1f65e9225e06972af54e87d09235c5e5e7823185
pizzaminded <pizzaminded@users.noreply.github.com>
2026-08-15 23:55:58 +0200

    bridge-twig: add implementation for config and extension configs

---
+++ src/Jadob/Bridge/Twig/ServiceProvider/ViteManifestExtensionConfig.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Jadob\Bridge\Twig\ServiceProvider;
+
+final class ViteManifestExtensionConfig
+{
+    public function __construct(
+        private(set) ?string $manifestLocation = null,
+    )
+    {
+    }
+
+    /**
+     * Must be relative to project root.
+     * @param string $location
+     * @return $this
+     */
+    public function withManifestLocation(
+        string $location,
+    ): self
+    {
+        $this->manifestLocation = $location;
+        return $this;
+    }
+}
