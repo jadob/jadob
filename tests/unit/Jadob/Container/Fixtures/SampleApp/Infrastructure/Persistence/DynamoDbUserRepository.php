b4e6d372a2e4fec1d0497b676d4a7ecd2ae9d344
pizzaminded <pizzaminded@users.noreply.github.com>
2026-08-16 23:22:19 +0200

    test: enhance fixture to be used in tests

--- tests/unit/Jadob/Container/Fixtures/SampleApp/Infrastructure/Persistence/DynamoDbUserRepository.php
+++ tests/unit/Jadob/Container/Fixtures/SampleApp/Infrastructure/Persistence/DynamoDbUserRepository.php
@@ -4,11 +4,12 @@ namespace Jadob\Container\Fixtures\SampleApp\Infrastructure\Persistence;
 
 use Jadob\Container\Fixtures\SampleApp\Domain\Repository\UserRepositoryInterface;
 use Jadob\Container\Fixtures\SampleApp\Infrastructure\Database\DynamoDbClient;
+use Jadob\Contracts\DependencyInjection\Attribute\InjectService;
 
-class DynamoDbUserRepository implements UserRepositoryInterface
+final class DynamoDbUserRepository implements UserRepositoryInterface
 {
     public function __construct(
-        private DynamoDbClient $client
+        #[InjectService(DynamoDbClient::class)] private DynamoDbClient $client
     )
     {
     }
