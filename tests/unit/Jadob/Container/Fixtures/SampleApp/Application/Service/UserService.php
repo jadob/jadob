94b936e7548a26850fcfe0618239c12b7b785fdf
pizzaminded <pizzaminded@users.noreply.github.com>
2026-09-19 23:15:04 +0200

    [container] reconfigure fixtures

--- tests/unit/Jadob/Container/Fixtures/SampleApp/Application/Service/UserService.php
+++ tests/unit/Jadob/Container/Fixtures/SampleApp/Application/Service/UserService.php
@@ -1,12 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Jadob\Container\Fixtures\SampleApp\Application\Service;
 
 use Jadob\Container\Fixtures\SampleApp\Domain\Repository\UserRepositoryInterface;
+use Jadob\Container\Fixtures\SampleApp\Infrastructure\Persistence\PostgresUserRepository;
+use Jadob\Contracts\DependencyInjection\Attribute\InjectService;
 
 class UserService
 {
     public function __construct(
+        #[InjectService(PostgresUserRepository::class)]
         private UserRepositoryInterface $repository
     )
     {
--- tests/unit/Jadob/Container/Fixtures/SampleApp/Infrastructure/Email/UserMailerService.php
+++ tests/unit/Jadob/Container/Fixtures/SampleApp/Infrastructure/Email/UserMailerService.php
@@ -1,17 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Jadob\Container\Fixtures\SampleApp\Infrastructure\Email;
 
 use Jadob\Container\Fixtures\SampleApp\Application\Service\UserNotificationServiceInterface;
 
 class UserMailerService implements UserNotificationServiceInterface
 {
-
     public function __construct(
         private string $smtpHost,
         private int $smtpPort,
-        private int $smtpUser,
-        private int $smtpPass,
+        private string $smtpUser,
+        private string $smtpPass,
     )
     {
     }
